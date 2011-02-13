50b2e1237289679573b2cbf484fb4884bf22cce4
Jürg Lehni <juerg@scratchdisk.com>
2011-02-13 16:24:55 +0000

    Add comments to Path Bounds tests.

--- test/tests/Path_Bounds.js
+++ test/tests/Path_Bounds.js
@@ -7,10 +7,12 @@ test('path.bounds', function() {
 		new Segment(new Point(248, 320), new Point(-42, -74), new Point(42, 74)),
 		new Segment(new Point(205, 420.94482421875), new Point(66.7890625, -12.72802734375), new Point(-79, 15.05517578125))
 	]);
+	// Test both closed and open paths, as the bounds for them differ
 	path.closed = false;
 	var bounds = path.bounds;
 	compareRectangles(bounds, { x: 121, y: 275.06796, width: 149.49304, height: 145.87686 });
 
+	// Test both closed and open paths, as the bounds for them differ
 	path.closed = true;
 	var bounds = path.bounds;
 	compareRectangles(bounds, { x: 114.82726, y: 275.06796, width: 155.66579, height: 148.12778 });
