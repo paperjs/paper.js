1edd4b4e211baf87ead4bbaba9db83a5e44104d5
Jürg Lehni <juerg@scratchdisk.com>
2011-02-14 01:25:35 +0000

    Add additional test for setting bounds.

--- test/tests/Path_Bounds.js
+++ test/tests/Path_Bounds.js
@@ -21,7 +21,11 @@ test('path.bounds', function() {
 	path.scale(0.5, 0.5);
 	compareRectangles(path.bounds, { x: 153.7437, y: 312.09976, width: 77.8329, height: 74.06381 });
 
-	// Now move the path to another position and check bounds again
+	// Move the path to another position and check bounds
 	path.position = [100, 100];
 	compareRectangles(path.bounds, { x: 61.08355, y: 62.96797, width: 77.83289, height: 74.06384 });
+
+	// Set new bounds and check segment list as result of resizing / positioning
+	path.bounds = { x: 100, y: 100, width: 200, height: 200 };
+	compareSegmentLists(path.segments, [{ point: { x: 107.93066, y: 179.56982 }, handleIn: { x: -24.41211, y: 51.30664 }, handleOut: { x: 39.52734, y: -83.08447 } }, { point: { x: 271.10107, y: 160.66553 }, handleIn: { x: -53.96289, y: -99.9126 }, handleOut: { x: 53.96143, y: 99.91406 } }, { point: { x: 215.85303, y: 296.96045 }, handleIn: { x: 85.81299, y: -17.18555 }, handleOut: { x: -101.49854, y: 20.32861 } }])
 });
