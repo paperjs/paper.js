3e2b4adc27ba4da47c5076abdf0919a5940d0db9
Jonathan Puckey <me@jonathanpuckey.com>
2011-02-16 18:55:12 +0100

    Layer: implement moveAbove / moveBelow / nextSibling / previousSibling

--- src/item/Layer.js
+++ src/item/Layer.js
@@ -3,13 +3,69 @@ Layer = Item.extend({
 
 	initialize: function() {
 		this.children = [];
-		this.document = this.parent = Paper.document;
+		this.document = Paper.document;
 		this.document.layers.push(this);
 		this.activate();
 	},
 
 	getIndex: function() {
 		return !this.parent ? this.document.layers.indexOf(this) : this.base();
+	},
+	
+	/**
+	* Removes the layer from its document's layers list
+	* or its parent's children list.
+	*/
+	removeFromParent: function() {
+		if(!this.parent) {
+			this.document.layers.splice(this.index, 1);
+		} else {
+			this.base();
+		}
+	},
+	
+	moveAbove: function(item) {
+		// if the item is a layer and contained within Document#layers
+		if(item instanceof Layer && !item.parent) {
+			this.removeFromParent();
+			item.document.layers.splice(item.index + 1, 0, this);
+			this.document = item.document;
+			return true;
+		} else {
+			this.base(item);
+		}
+	},
+	
+	moveBelow: function(item) {
+		// if the item is a layer and contained within Document#layers
+		if(item instanceof Layer && !item.parent) {
+			this.removeFromParent();
+			item.document.layers.splice(item.index - 1, 0, this);
+			this.document = item.document;
+			return true;
+		} else {
+			this.base(item);
+		}
+	},
+	
+	getNextSibling: function() {
+		if(!this.parent) {
+			var index = this.index + 1;
+			if (index < this.document.layers.length)
+				return this.document.layers[index];
+		} else {
+			return this.base();
+		}
+	},
+	
+	getPreviousSibling: function() {
+		if(!this.parent) {
+			var index = this.index - 1;
+			if (index <= 0)
+				return this.document.layers[index];
+		} else {
+			return this.base();
+		}
 	},
 
 	activate: function() {
