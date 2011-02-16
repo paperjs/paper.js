c0884473b59bdf11246cadf2425286cdd4258fa3
Jonathan Puckey <me@jonathanpuckey.com>
2011-02-17 00:34:16 +0100

    Add a test for Group#getBounds().

--- test/tests/Group.js
+++ test/tests/Group.js
@@ -13,3 +13,11 @@ test('new Group([item])', function() {
 	equals(doc.activeLayer.children.length == 1, true);
 	equals(group.children[0] == path, true);
 });
+
+test('Group bounds', function() {
+	var doc = new Doc();
+	var path = new Path.Circle([150, 150], 60);
+	var secondPath = new Path.Circle([175, 175], 85);
+	var group = new Group([path, secondPath]);
+	compareRectangles(group.bounds, { x: 90, y: 90, width: 170, height: 170 });
+});
