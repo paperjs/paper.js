8a0a0712cd0505470ed2b9e8afa161e93b61b640
Jürg Lehni <juerg@scratchdisk.com>
2011-02-16 00:18:58 +0000

    Add Layer#index to override Item#index.

--- src/item/Layer.js
+++ src/item/Layer.js
@@ -1,11 +1,17 @@
 Layer = Item.extend({
+	beans: true,
+
 	initialize: function() {
 		this.children = [];
 		this.document = this.parent = Paper.document;
 		this.document.layers.push(this);
 		this.activate();
 	},
-	
+
+	getIndex: function() {
+		return !this.parent ? this.document.layers.indexOf(this) : this.base();
+	},
+
 	activate: function() {
 		this.document.activeLayer = this;
 	},
