dbbefa952608cf0729f0f3cd657c34f8fa4a4789
Jürg Lehni <juerg@scratchdisk.com>
2011-03-02 16:22:26 +0000

    index on master: fd4b7b7 Include DocumentView.js in all examples and tests.

---
+++ src/path/Curve.js
@@ -0,0 +1,20 @@
+Curve = Base.extend({
+	initialize: function() {
+	},
+	
+	
+	statics: {
+		read: function(args, index) {
+			var index = index || 0, length = args.length - index;
+			if (length == 1 && args[index] instanceof Curve) {
+				return args[index];
+			} else if (length != 0) {
+				var curve = new Curve(Curve.dont);
+				curve.initialize.apply(curve, index > 0
+						? Array.prototype.slice.call(args, index) : args);
+				return curve;
+			}
+			return null;
+		}
+	}
+});
