06e870f69c2831c3e1e90b53445c74ed09930cdd
Jürg Lehni <juerg@scratchdisk.com>
2011-03-05 14:26:16 +0000

    Improve some comments.

--- src/util/Events.js
+++ src/util/Events.js
@@ -5,8 +5,8 @@ var Events = {
 			if (obj.addEventListener) {
 				obj.addEventListener(type, func, false);
 			} else if (obj.attachEvent) {
-				// Make a bound closure that calls on the write object and
-				// passes the event object.
+				// Make a bound closure that calls on the right object and
+				// passes on the global event object as a parameter.
 				obj.attachEvent('on' + type, func.bound = function() {
 					func.call(obj, window.event);
 				});
@@ -20,7 +20,7 @@ var Events = {
 			if (obj.removeEventListener) {
 				obj.removeEventListener(type, func, false);
 			} else if (obj.detachEvent) {
-				// Remove the bound function instead of func itself
+				// Remove the bound closure instead of func itself
 				obj.detachEvent('on' + type, func.bound);
 			}
 		}
