bbfd8eea7395ac62b401228b0e5eefba86183cc8
Jürg Lehni <juerg@scratchdisk.com>
2011-03-07 02:29:17 +0000

    Add comments as a reference for values produced by different precisions.

--- test/tests/Path_Length.js
+++ test/tests/Path_Length.js
@@ -11,6 +11,11 @@ test('path.length', function() {
 		var length = path.length;
 	}
 	window.console.log(Date.now() - t, length);
+	// ai: 172.10122680664062
+	//  8: 172.10094440399325
+	//  7: 172.10190407418446
+	//  6: 172.09823801587845
+	//  5: 172.1100076851322
 	compareNumbers(length, 172.10122680664062);
 
 	var t = Date.now(), c = 1000;
@@ -18,5 +23,9 @@ test('path.length', function() {
 		var param = path.curves[0].getParameter(length / 4);
 	}
 	window.console.log(Date.now() - t, param);
-	compareNumbers(param, 0.2255848449949521);
+	// ai: 0.2255849553116685
+	//  8: 0.22558507711602457
+	//  5: 0.22558507714028128
+	//	4: 0.22558508917324532
+	compareNumbers(param, 0.2255849553116685);
 });
