798bcd709a4f6b0d608753bebcd5d18ab850d4e9
Jürg Lehni <juerg@scratchdisk.com>
2011-03-07 23:11:41 +0000

    Since Firefox does not seem to support the <base> tag for script loading, roll our own solution through the load.js calls.

--- src/load.js
+++ src/load.js
@@ -1,56 +1,57 @@
-var id = '?' + Math.random();
+var id = '?' + Math.random(),
+	base = loadBase || '';
 load(
-	'lib/bootstrap.js' + id,
-	'lib/parse-js.js' + id
+	base + 'lib/bootstrap.js' + id,
+	base + 'lib/parse-js.js' + id
 ).then(
-	'src/paper.js' + id
+	base + 'src/paper.js' + id
 ).then(
-	'src/basic/Point.js' + id,
-	'src/basic/Size.js' + id,
-	'src/basic/Rectangle.js' + id,
-	'src/basic/Matrix.js' + id,
-	'src/basic/Line.js' + id
+	base + 'src/basic/Point.js' + id,
+	base + 'src/basic/Size.js' + id,
+	base + 'src/basic/Rectangle.js' + id,
+	base + 'src/basic/Matrix.js' + id,
+	base + 'src/basic/Line.js' + id
 ).then(
-	'src/document/DocumentView.js' + id,
-	'src/document/Document.js' + id,
-	'src/document/Symbol.js' + id
+	base + 'src/document/DocumentView.js' + id,
+	base + 'src/document/Document.js' + id,
+	base + 'src/document/Symbol.js' + id
 ).then(
-	'src/item/Item.js' + id
+	base + 'src/item/Item.js' + id
 ).then(
-	'src/item/Group.js' + id,
-	'src/item/Raster.js' + id,
-	'src/item/PlacedSymbol.js' + id,
-	'src/item/PathStyle.js' + id,
-	'src/path/PathItem.js' + id,
-	'src/path/Segment.js' + id,
-	'src/path/Curve.js' + id
+	base + 'src/item/Group.js' + id,
+	base + 'src/item/Raster.js' + id,
+	base + 'src/item/PlacedSymbol.js' + id,
+	base + 'src/item/PathStyle.js' + id,
+	base + 'src/path/PathItem.js' + id,
+	base + 'src/path/Segment.js' + id,
+	base + 'src/path/Curve.js' + id
 ).then(
 	// Needs Group
-	'src/item/Layer.js' + id,
+	base + 'src/item/Layer.js' + id,
 	// Needs PathItem
-	'src/path/Path.js' + id,
-	'src/path/CompoundPath.js' + id
+	base + 'src/path/Path.js' + id,
+	base + 'src/path/CompoundPath.js' + id
 ).then(
-	'src/path/Path.Constructors.js' + id,
-	'src/color/Color.js' + id
+	base + 'src/path/Path.Constructors.js' + id,
+	base + 'src/color/Color.js' + id
 ).then(
-	'src/color/RGBColor.js' + id,
-	'src/color/GrayColor.js' + id,
-	'src/color/GradientColor.js' + id,
-	'src/color/Gradient.js' + id,
-	'src/color/GradientStop.js' + id
+	base + 'src/color/RGBColor.js' + id,
+	base + 'src/color/GrayColor.js' + id,
+	base + 'src/color/GradientColor.js' + id,
+	base + 'src/color/Gradient.js' + id,
+	base + 'src/color/GradientStop.js' + id
 ).then(
-	'src/tool/ToolEvent.js' + id,
-	'src/tool/ToolHandler.js' + id
+	base + 'src/tool/ToolEvent.js' + id,
+	base + 'src/tool/ToolHandler.js' + id
 ).then(
 	// Requires ToolHandler
-	'src/tool/Tool.js' + id,
-	'src/util/BlendMode.js' + id,
-	'src/util/CanvasProvider.js' + id,
-	'src/util/Numerical.js' + id,
-	'src/util/Events.js' + id
+	base + 'src/tool/Tool.js' + id,
+	base + 'src/util/BlendMode.js' + id,
+	base + 'src/util/CanvasProvider.js' + id,
+	base + 'src/util/Numerical.js' + id,
+	base + 'src/util/Events.js' + id
 ).then(
-	'src/util/PaperScript.js' + id
+	base + 'src/util/PaperScript.js' + id
 ).thenRun(
 	function() {
 		PaperScript.install();
