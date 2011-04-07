5f05c85b4274943f9876c7b6ddaead0686097330
Jonathan Puckey <me@jonathanpuckey.com>
2011-04-07 17:12:00 +0200

    Gradient: rename midPoint to rampPoint.

--- src/color/Gradient.js
+++ src/color/Gradient.js
@@ -38,18 +38,18 @@ var Gradient = this.Gradient = Base.extend({
 					'Gradient stop list needs to contain at least two stops.');
 		if(!(stops[0] instanceof GradientStop)) {
 			for(var i = 0, l = stops.length; i < l; i++) {
-				var midPoint;
+				var rampPoint;
 				var stop = stops[i];
-				// If it is an array, the second argument is the midPoint:
+				// If it is an array, the second argument is the rampPoint:
 				if(stop.length) {
-					midPoint = stop[1];
+					rampPoint = stop[1];
 					stop = stop[0];
 				} else {
 				// Otherwise stops is an array of colors, and we need to
 				// calculate the midPoint:
-					midPoint = i / (l - 1);
+					rampPoint = i / (l - 1);
 				}
-				stops[i] = new GradientStop(stop, midPoint);
+				stops[i] = new GradientStop(stop, rampPoint);
 			}
 		}
 		this._stops = stops;
