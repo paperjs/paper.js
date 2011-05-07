bbbc4012b5f0df0a934737a0c02fc17fe0f6d34f
Jürg Lehni <juerg@scratchdisk.com>
2011-05-07 16:11:05 +0100

    Add Base.splice() to handle lists of items with _index referneces and use it for paper.documents and Item#children.

--- src/paper.js
+++ src/paper.js
@@ -79,6 +79,38 @@ Base.inject({
 		return res;
 	},
 
+	/**
+	 * Utility function for adding and removing items from a list of which
+	 * each entry keeps a reference to its index in the list in the private
+	 * _index property. Used for paper.documents and Item#children.
+	 */
+	splice: function(list, items, index, remove) {
+		var amount = items && items.length,
+			append = index === undefined;
+		index = append ? list.length : index;
+		// Update _index on the items to be added first.
+		for (var i = 0; i < amount; i++) {
+			items[i]._index = index + i;
+		}
+		if (append) {
+			// Append them all at the end by using push
+			list.push.apply(list, items);
+			// Nothing removed, and nothing to adjust above
+			return [];
+		} else {
+			// Insert somewhere else and/or remove
+			var args = [index, remove];
+			if (items)
+				args.push.apply(args, items);
+			var removed = list.splice.apply(list, args);
+			// Adjust the indices of the items above.
+			for (var i = index + amount, l = list.length; i < l; i++) {
+				list[i]._index = i;
+			}
+			return removed;
+		}
+	},
+
 	capitalize: function(str) {
 		return str.replace(/\b[a-z]/g, function(match) {
 			return match.toUpperCase();
