683a22bfa6e3bff8d414017ff8b8e535ed04d8d2
Jürg Lehni <juerg@scratchdisk.com>
2011-05-07 11:24:01 +0100

    Include util files at the end of the library, not the beginning.

--- src/paper.js
+++ src/paper.js
@@ -90,16 +90,6 @@ Base.inject({
 	}
 });
 
-//#ifdef BROWSER
-//#include "browser/Element.js"
-//#include "browser/Event.js"
-//#endif // BROWSER
-
-//#include "util/CanvasProvider.js"
-//#include "util/Numerical.js"
-//#include "util/PaperScript.js"
-//#include "util/BlendMode.js"
-
 //#include "basic/Point.js"
 //#include "basic/Size.js"
 //#include "basic/Rectangle.js"
@@ -137,6 +127,16 @@ Base.inject({
 //#include "tool/ToolEvent.js"
 //#include "tool/ToolHandler.js"
 //#include "tool/Tool.js"
+
+//#ifdef BROWSER
+//#include "browser/Element.js"
+//#include "browser/Event.js"
+//#endif // BROWSER
+
+//#include "util/CanvasProvider.js"
+//#include "util/Numerical.js"
+//#include "util/PaperScript.js"
+//#include "util/BlendMode.js"
 
 //#ifdef BROWSER
 //#include "ui/Key.js"
