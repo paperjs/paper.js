37f982e53fc908f8bc40904ece10708a10de253f
Jürg Lehni <juerg@scratchdisk.com>
2011-05-08 13:46:31 +0100

    Remove unused lookup table.

--- src/ui/KeyEvent.js
+++ src/ui/KeyEvent.js
@@ -15,23 +15,6 @@
  */
 
 var KeyEvent = this.KeyEvent = Event.extend(new function() {
-	var keys = {
-		 8: 'backspace',
-		13: 'enter',
-		16: 'shift',
-		17: 'control',
-		19: 'option', // was alt
-		20: 'capsLock',
-		27: 'escape',
-		32: 'space',
-		37: 'left',
-		38: 'up',
-		39: 'right',
-		40: 'down',
-		46: 'delete',
-		91: 'command'
-	};
-
 	return {
 		initialize: function(down, keyCode, character, event) {
 			this.base(event);
