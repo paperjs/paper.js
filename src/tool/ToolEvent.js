04ff593adcd5c4cfe7293b958e28bbcb5e482bfe
Jürg Lehni <juerg@scratchdisk.com>
2011-05-08 15:33:00 +0100

    Have Events print modifiers correctly in #toString().

--- src/tool/ToolEvent.js
+++ src/tool/ToolEvent.js
@@ -212,7 +212,7 @@ var ToolEvent = this.ToolEvent = Base.extend({
 		return '{ type: ' + this.type 
 				+ ', point: ' + this.point
 				+ ', count: ' + this.count
-				+ ', modifiers: ' + this.modifiers
+				+ ', modifiers: ' + this.getModifiers(true)
 				+ ' }';
 	}
 });
