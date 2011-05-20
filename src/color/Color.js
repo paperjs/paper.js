2afd2213a9748f4a0cbafd7a4ad2ee1104b56e84
Jürg Lehni <juerg@scratchdisk.com>
2011-05-20 08:08:26 +0100

    Move isHue check outside of setter.

--- src/color/Color.js
+++ src/color/Color.js
@@ -218,13 +218,14 @@ var Color = this.Color = Base.extend(new function() {
 					// Automatically produce the _components field, adding alpha
 					src._components = comps.concat(['alpha']);
 					Base.each(comps, function(name) {
-						var part = Base.capitalize(name),
+						var isHue = name === 'hue',
+							part = Base.capitalize(name),
 							name = '_' + name;
 						this['get' + part] = function() {
 							return this[name];
 						};
 						this['set' + part] = function(value) {
-							this[name] = name === '_hue'
+							this[name] = isHue
 								// Keep negative values within modulo 360 too:
 								? ((value % 360) + 360) % 360
 								// All other values are 0..1
