0a27879a23011593480042cc5fd0bfe378ddc643
Jonathan Puckey <me@jonathanpuckey.com>
2011-05-23 19:56:18 +0200

    Add KeyEvent documentation.

--- src/ui/KeyEvent.js
+++ src/ui/KeyEvent.js
@@ -16,13 +16,50 @@
 
 var KeyEvent = this.KeyEvent = Event.extend(new function() {
 	return {
+		/** @lends KeyEvent# */
+
+		beans: true,
+
+		/**
+		 * @name KeyEvent
+		 * @constructor
+		 * 
+		 * @class KeyEvent The KeyEvent object is received by the {@link Tool}'s
+		 * keyboard handlers {@link Tool#onKeyDown}, {@link Tool#onKeyUp},
+		 * The KeyEvent object is the only parameter passed to these functions
+		 * and contains information about the keyboard event.
+		 */
 		initialize: function(down, key, character, event) {
 			this.base(event);
 			this.type = down ? 'keydown' : 'keyup';
 			this.key = key;
 			this.character = character;
 		},
-	
+
+		/**
+		 * The type of key event.
+		 * 
+		 * @return {string} the type of key event: 'keydown' or 'keyup'
+		 * @name KeyEvent#type
+		 */
+
+		/**
+		 * The string character of the key that caused this key event.
+		 * 
+		 * @name KeyEvent#character
+		 * @return {string} the key character.
+		 */
+
+		/**
+		 * Specifies the virtual key code of the key that caused this key event.
+		 * 
+		 * @name KeyEvent#key
+		 * @return the virtual key code.
+		 */
+
+		/**
+		 * @return {string} A string representation of the key event.
+		 */
 		toString: function() {
 			return '{ type: ' + this.type 
 					+ ', key: ' + this.key
