54b40faf5d0d8fd6659f05fd93b869e9c7e9523e
Jonathan Puckey <me@jonathanpuckey.com>
2011-05-28 15:43:26 +0200

    jsdoc: clean up templates and publish.js.

--- build/jsdoc-toolkit/templates/jsdoc/publish.js
+++ build/jsdoc-toolkit/templates/jsdoc/publish.js
@@ -8,11 +8,6 @@ var Operator = new function() {
 		divide: 'Division', equals: 'Comparison', modulo: 'Modulo'
 	};
 	
-	var operatorClasses = {
-		Point: true,
-		Size: true
-	};
-	
 	return {
 		isOperator: function(symbol) {
 			// As a convention, only add non static bean properties to
@@ -57,74 +52,56 @@ var Helpers = {
 			}
 		}
 		return id.join('-');
+	},
+	
+	isaClass: function(symbol) {
+		return symbol.is('CONSTRUCTOR') || symbol.isNamespace
 	}
 };
 
-var templates;
 /** Called automatically by JsDoc Toolkit. */
 function publish(symbolSet) {
 	publish.conf = {  // trailing slash expected for dirs
-		ext:		 ".html",
-		outDir:	  JSDOC.opt.d || SYS.pwd+"../out/jsdoc/",
-		templatesDir: JSDOC.opt.t || SYS.pwd+"../templates/jsdoc/",
-		symbolsDir:  "symbols/",
-		srcDir:	  "symbols/src/"
+		ext: '.html',
+		outDir:	  JSDOC.opt.d || SYS.pwd + '../out/jsdoc/',
+		templateDir: JSDOC.opt.t || SYS.pwd + '../templates/jsdoc/',
+		resourcesDir:  (JSDOC.opt.t || SYS.pwd + '../templates/jsdoc/') + 'resources/',
+		symbolsDir:  'symbols/',
+		srcDir:	  'symbols/src/'
 	};
-	
-	templates = {
-		_class: new JSDOC.JsPlate(publish.conf.templatesDir + "class.tmpl"),
-		method: new JSDOC.JsPlate(publish.conf.templatesDir + "method.tmpl"),
-		property: new JSDOC.JsPlate(publish.conf.templatesDir + "property.tmpl"),
-		parameters: new JSDOC.JsPlate(publish.conf.templatesDir + "parameters.tmpl"),
-		operators: new JSDOC.JsPlate(publish.conf.templatesDir + "operators.tmpl"),
-		examples: new JSDOC.JsPlate(publish.conf.templatesDir + "examples.tmpl"),
-		constructor: new JSDOC.JsPlate(publish.conf.templatesDir + "constructor.tmpl")		
+
+	var templatesDir = publish.conf.templateDir + 'templates/';
+	publish.templates = {
+		_class: 'class.tmpl',
+		method: 'method.tmpl',
+		property: 'property.tmpl',
+		parameters: 'parameters.tmpl',
+		operators: 'operators.tmpl',
+		examples: 'examples.tmpl',
+		constructor: 'constructor.tmpl',
+		html: 'html.tmpl',
+		allClasses: 'allClasses.tmpl',
+		classesIndex: 'index.tmpl'
 	};
-	
-	// is source output is suppressed, just display the links to the source file
-	if (JSDOC.opt.s && defined(Link) && Link.prototype._makeSrcLink) {
-		Link.prototype._makeSrcLink = function(srcFilePath) {
-			return "&lt;"+srcFilePath+"&gt;";
-		}
+
+	for (var i in publish.templates) {
+		publish.templates[i] = new JSDOC.JsPlate(templatesDir +
+				publish.templates[i]);
 	}
 
 	// Copy over the static files
 	copyDirectory(
-		new java.io.File(publish.conf.templatesDir + 'resources/'),
+		new java.io.File(publish.conf.resourcesDir),
 		new java.io.File(publish.conf.outDir + 'resources/')
 	);
-	
+
 	// used to allow Link to check the details of things being linked to
 	Link.symbolSet = symbolSet;
 
-	// create the required templates
-	try {
-		var classTemplate = new JSDOC.JsPlate(publish.conf.templatesDir+"class.tmpl");
-		var classesTemplate = new JSDOC.JsPlate(publish.conf.templatesDir+"allclasses.tmpl");
-	}
-	catch(e) {
-		print("Couldn't create the required templates: "+e);
-		quit();
-	}
-	
-	// some ustility filters
-	function hasNoParent($) {return ($.memberOf == "")}
-	function isaFile($) {return ($.is("FILE"))}
-	function isaClass($) {return ($.is("CONSTRUCTOR") || $.isNamespace)}
-	
 	// get an array version of the symbolset, useful for filtering
-	var symbols = symbolSet.toArray();
-	
-	// create the hilited source code files
-	var files = JSDOC.opt.srcFiles;
-	 	for (var i = 0, l = files.length; i < l; i++) {
-	 		var file = files[i];
-	 		var srcDir = publish.conf.outDir + "symbols/src/";
-		makeSrcFile(file, srcDir);
-	 	}
- 	
- 	// get a list of all the classes in the symbolset
- 	var classes = symbols.filter(isaClass).sort(makeSortby("alias"));
+	var symbols = symbolSet.toArray(),
+		files = JSDOC.opt.srcFiles,
+ 		classes = symbols.filter(Helpers.isaClass).sort(makeSortby('alias'));
 	
 	// create a filemap in which outfiles must be to be named uniquely, ignoring case
 	if (JSDOC.opt.u) {
@@ -133,18 +110,20 @@ function publish(symbolSet) {
 		for (var i = 0, l = classes.length; i < l; i++) {
 			var lcAlias = classes[i].alias.toLowerCase();
 			
-			if (!filemapCounts[lcAlias]) filemapCounts[lcAlias] = 1;
-			else filemapCounts[lcAlias]++;
+			if (!filemapCounts[lcAlias]) {
+				filemapCounts[lcAlias] = 1;
+			} else {
+				filemapCounts[lcAlias]++;
+			}
 			
-			Link.filemap[classes[i].alias] = 
-				(filemapCounts[lcAlias] > 1)?
-				lcAlias+"_"+filemapCounts[lcAlias] : lcAlias;
+			Link.filemap[classes[i].alias] =  (filemapCounts[lcAlias] > 1) ?
+				lcAlias + '_' + filemapCounts[lcAlias] : lcAlias;
 		}
 	}
 	
 	// create a class index, displayed in the left-hand column of every class page
-	Link.base = "../";
- 	publish.classesIndex = classesTemplate.process(classes); // kept in memory
+	Link.base = '../';
+ 	publish.conf.classesIndex = publish.templates.allClasses.process(classes); // kept in memory
 	
 	// create each of the class pages
 	for (var i = 0, l = classes.length; i < l; i++) {
@@ -158,57 +137,21 @@ function publish(symbolSet) {
 		}
 		
 		Link.currentSymbol= symbol;
-		var output = "";
-		output = classTemplate.process(symbol);
-		
-		IO.saveFile(publish.conf.outDir+"symbols/", ((JSDOC.opt.u)? Link.filemap[symbol.alias] : symbol.alias) + publish.conf.ext, output);
+		var html = publish.templates.html.process({
+			content: publish.templates._class.process(symbol),
+			title: symbol.alias
+		})
+		var name = ((JSDOC.opt.u)? Link.filemap[symbol.alias] : symbol.alias)
+				+ publish.conf.ext;
+		IO.saveFile(publish.conf.outDir + 'symbols/', name, html);
 	}
 	
 	// regenerate the index with different relative links, used in the index pages
-	Link.base = "";
-	publish.classesIndex = classesTemplate.process(classes);
+	Link.base = '';
+	publish.conf.classesIndex = publish.templates.allClasses.process(classes);
 	
-	// create the class index page
-	try {
-		var classesindexTemplate = new JSDOC.JsPlate(publish.conf.templatesDir+"index.tmpl");
-	}
-	catch(e) { print(e.message); quit(); }
-	
-	var classesIndex = classesindexTemplate.process(classes);
-	IO.saveFile(publish.conf.outDir, "index"+publish.conf.ext, classesIndex);
-	classesindexTemplate = classesIndex = classes = null;
-	
-	// create the file index page
-	try {
-		var fileindexTemplate = new JSDOC.JsPlate(publish.conf.templatesDir+"allfiles.tmpl");
-	}
-	catch(e) { print(e.message); quit(); }
-	
-	var documentedFiles = symbols.filter(isaFile); // files that have file-level docs
-	var allFiles = []; // not all files have file-level docs, but we need to list every one
-	
-	for (var i = 0; i < files.length; i++) {
-		allFiles.push(new JSDOC.Symbol(files[i], [], "FILE", new JSDOC.DocComment("/** */")));
-	}
-	
-	for (var i = 0; i < documentedFiles.length; i++) {
-		var offset = files.indexOf(documentedFiles[i].alias);
-		allFiles[offset] = documentedFiles[i];
-	}
-		
-	allFiles = allFiles.sort(makeSortby("name"));
-
-	// output the file index page
-	var filesIndex = fileindexTemplate.process(allFiles);
-	IO.saveFile(publish.conf.outDir, "files"+publish.conf.ext, filesIndex);
-	fileindexTemplate = filesIndex = files = null;
-}
-
-
-/** Just the first sentence (up to a full stop). Should not break on dotted variable names. */
-function summarize(desc) {
-	if (typeof desc != "undefined")
-		return desc.match(/([\w\W]+?\.)[^a-z0-9_$]/i)? RegExp.$1 : desc;
+	var classesIndex = publish.templates.classesIndex.process(classes);
+	IO.saveFile(publish.conf.outDir, 'index' + publish.conf.ext, classesIndex);
 }
 
 /** Make a symbol sorter by some attribute. */
@@ -226,38 +169,18 @@ function makeSortby(attribute) {
 
 /** Pull in the contents of an external file at the given path. */
 function include(path) {
-	var path = publish.conf.templatesDir+path;
+	var path = publish.conf.templateDir + path;
 	return IO.readFile(path);
-}
-
-/** Turn a raw source file into a code-hilited page in the docs. */
-function makeSrcFile(path, srcDir, name) {
-	if (JSDOC.opt.s) return;
-	
-	if (!name) {
-		name = path.replace(/\.\.?[\\\/]/g, "").replace(/[\\\/]/g, "_");
-		name = name.replace(/\:/g, "_");
-	}
-	
-	var src = {path: path, name:name, charset: IO.encoding, hilited: ""};
-	
-	if (defined(JSDOC.PluginManager)) {
-		JSDOC.PluginManager.run("onPublishSrc", src);
-	}
-
-	if (src.hilited) {
-		IO.saveFile(srcDir, name+publish.conf.ext, src.hilited);
-	}
 }
 
 /** Build output for displaying function parameters. */
 function makeSignature(params) {
-	if (!params) return "()";
+	if (!params) return '()';
 	var postString = '';
 	var first = true;
 	params = params.filter(
 		function($) {
-			return $.name.indexOf(".") == -1; // don't show config params in signature
+			return $.name.indexOf('.') == -1; // don't show config params in signature
 		}
 	);
 	var signature = '';
@@ -360,11 +283,6 @@ function processInlineTags(str, param) {
 function stripTags(str, tag) {
 	var tag = tag || '.*?'; // Default: all tags
 	return str.replace(new RegExp('<' + tag + '>|</' + tag + '>', 'g'), '');
-}
-
-function copyStatic(dir) {
-	var dir = publish.conf.templatesDir + 'resources/';
-	
 }
 
 function copyDirectory(sourceLocation, targetLocation) {
