1e72f79ad51d65bff071661c75774af946c05c47
Jürg Lehni <juerg@scratchdisk.com>
2011-06-08 13:36:46 +0100

    jsdoc: Add asCode parameter to Link() constructor, to define if it should be wrapped in <tt> or not.

--- build/jsdoc-toolkit/app/frame/Link.js
+++ build/jsdoc-toolkit/app/frame/Link.js
@@ -1,7 +1,7 @@
 /** Handle the creation of HTML links to documented symbols.
 	@constructor
 */
-function Link() {
+function Link(asCode) {
 	this.alias = "";
 	this.src = "";
 	this.file = "";
@@ -9,6 +9,7 @@ function Link() {
 	this.innerName = "";
 	this.classLink = false;
 	this.targetName = "";
+	this.asCode = asCode;
 	
 	this.target = function(targetName) {
 		if (defined(targetName)) this.targetName = targetName;
@@ -172,7 +173,12 @@ Link.prototype._makeSymbolLink = function(alias, parameters) {
 		var linkName = link.linkPath.replace(/^[^#]+#/, '');
 		onClick = " onclick=\"return toggleMember('" + linkName + "', true);\"";
 	}
-	var html = "<a href=\""+link.linkPath+link.linkInner+"\""+target+onClick+"><tt>"+link.linkText+"</tt></a>";
+	var text = link.linkText;
+	// TODO: tt is gone in HTML5. Instead we should give these links and code 
+	// elements a special class so they can be styled through CSS.
+	if (this.asCode)
+		text = "<tt>" + text + "</tt>";
+	var html = "<a href=\""+link.linkPath+link.linkInner+"\""+target+onClick+">" + text + "</a>";
 	if (this.prefix || this.suffix)
 		html = (this.prefix || '') + html + (this.suffix || '');
 	return html;
