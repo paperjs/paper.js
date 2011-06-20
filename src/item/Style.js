456bf7f2a9cf913fd957db3d387b3211659892b3
Jürg Lehni <juerg@scratchdisk.com>
2011-06-20 15:21:42 +0100

    Move more getter / setter injection functionality from PathStyle to Style, to be shared among all Style classes.

--- src/item/Style.js
+++ src/item/Style.js
@@ -19,7 +19,6 @@
  * PargraphStyle.
  */
 var Style = Item.extend({
-
 	initialize: function(style) {
 		// If the passed style object is also a Style, clone its clonable
 		// fields rather than simply copying them.
@@ -44,17 +43,69 @@ var Style = Item.extend({
 			// Inject style getters and setters into the 'owning' class, which
 			// redirect calls to the linked style objects through their internal
 			// property on the instances of that class, as defined by _style.
-			var style = src._style;
+			var styleKey = src._style,
+				flags = src._flags || {};
 			src._owner.inject(Base.each(src._defaults, function(value, key) {
-				var part = Base.capitalize(key),
+				var isColor = !!key.match(/Color$/),
+					part = Base.capitalize(key),
 					set = 'set' + part,
 					get = 'get' + part;
+				// Simply extend src with these getters and setters, to be
+				// injected into this class using this.base() further down.
+				src[set] = function(value) {
+					var children = this._item && this._item._children;
+					value = isColor ? Color.read(arguments) : value;
+					if (children) {
+						for (var i = 0, l = children.length; i < l; i++)
+							children[i][styleKey][set](value);
+					} else {
+						var old = this['_' + key];
+						if (old != value && !(old && old.equals
+									&& old.equals(value))) {
+							this['_' + key] = value;
+							if (isColor) {
+								if (old)
+									old._removeOwner(this._item);
+								if (value)
+									value._addOwner(this._item);
+							}
+							if (this._item)
+								this._item._changed(flags[key] || Change.STYLE);
+						}
+					}
+					return this;
+				};
+				src[get] = function() {
+					var children = this._item && this._item._children,
+						style;
+					// If this item has children, walk through all of them and
+					// see if they all have the same style.
+					if (!children)
+						return this['_' + key];
+					for (var i = 0, l = children.length; i < l; i++) {
+						var childStyle = children[i][styleKey][get]();
+						if (!style) {
+							style = childStyle;
+						} else if (style != childStyle && !(style
+								&& style.equals && style.equals(childStyle))) {
+							// If there is another item with a different
+							// style, the style is not defined:
+							// PORT: Change this in Scriptographer
+							// (currently returns null)
+							return undefined;
+						}
+					}
+					return style;
+				};
+				// Style-getters and setters for owner class:
+				// 'this' = the Base.each() side-car = the object that is
+				// returned from Base.each and injected into _owner above:
 				this[set] = function(value) {
-					this[style][set](value);
+					this[styleKey][set](value);
 					return this;
 				};
 				this[get] = function() {
-					return this[style][get]();
+					return this[styleKey][get]();
 				};
 			}, {}));
 			return this.base(src);
