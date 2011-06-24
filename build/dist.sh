4759cb864f69250f0b06d3a7fa649cb9a518fbc9
Jürg Lehni <juerg@scratchdisk.com>
2011-06-24 20:12:48 +0100

    Add scripts for packaging paper.js for distribution.

---
+++ build/dist.sh
@@ -0,0 +1,17 @@
+# Paper.js
+#
+# This file is part of Paper.js, a JavaScript Vector Graphics Library,
+# based on Scriptographer.org and designed to be largely API compatible.
+# http://scriptographer.org/
+#
+# Copyright (c) 2011, Juerg Lehni & Jonathan Puckey
+# http://lehni.org/ & http://jonathanpuckey.com/
+#
+# All rights reserved. See LICENSE file for details.
+
+echo "Building paper.js"
+./build.sh
+echo "Building docs"
+./docs.sh
+echo "Zipping paperjs.zip"
+./zip.sh
