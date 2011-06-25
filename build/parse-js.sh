8952a6fac9342e479db4d4c822354516d31e45ee
Jürg Lehni <juerg@scratchdisk.com>
2011-06-25 22:34:59 +0200

    Add #!/bin/sh to all shell scripts.

--- build/parse-js.sh
+++ build/parse-js.sh
@@ -1,1 +1,18 @@
+#!/bin/sh
+
+# Paper.js
+#
+# This file is part of Paper.js, a JavaScript Vector Graphics Library,
+# based on Scriptographer.org and designed to be largely API compatible.
+# http://scriptographer.org/
+#
+# Copyright (c) 2011, Juerg Lehni & Jonathan Puckey
+# http://lehni.org/ & http://jonathanpuckey.com/
+#
+# All rights reserved. See LICENSE file for details.
+
+# Generate a paper.js file that uses load.js to directly load the library
+# through the seperate source files in the src directory. Very useful during
+# development of the library itself.
+
 ./preprocess.sh ../lib/parse-js.js ../lib/parse-js-min.js "" uglified
