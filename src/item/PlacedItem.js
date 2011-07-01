d95480bc22613748ca2b163a7f17143ae08231d7
Jürg Lehni <juerg@scratchdisk.com>
2011-07-01 12:01:32 +0200

    Remove trailing white spaces.

--- src/item/PlacedItem.js
+++ src/item/PlacedItem.js
@@ -1,26 +1,26 @@
 /*
  * Paper.js
- * 
+ *
  * This file is part of Paper.js, a JavaScript Vector Graphics Library,
  * based on Scriptographer.org and designed to be largely API compatible.
  * http://paperjs.org/
  * http://scriptographer.org/
- * 
+ *
  * Distributed under the MIT license. See LICENSE file for details.
- * 
+ *
  * Copyright (c) 2011, Juerg Lehni & Jonathan Puckey
  * http://lehni.org/ & http://jonathanpuckey.com/
- * 
+ *
  * All rights reserved.
  */
 
 /**
  * @name PlacedItem
- * 
+ *
  * @class The PlacedItem class is the base for any items that have a matrix
  * associated with them, describing their placement in the project, such as
  * {@link Raster} and {@link PlacedSymbol}.
- * 
+ *
  * @extends Item
  */
 var PlacedItem = this.PlacedItem = Item.extend(/** @lends PlacedItem# */{
