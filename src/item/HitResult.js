dfbe3135ad7c4aaa06fb074b6de027013b16098c
Jürg Lehni <juerg@scratchdisk.com>
2011-07-08 22:25:42 +0200

    Remove HitResult inheritance from CurveLocation, but have a #location property pointing to it.

--- src/item/HitResult.js
+++ src/item/HitResult.js
@@ -21,19 +21,12 @@
  *
  * @extends CurveLocation
  */
-HitResult = CurveLocation.extend(/** @lends HitResult# */{
-	initialize: function(type, item) {
-		this._type = type;
-		if (item instanceof CurveLocation) {
-			// If a CurveLocation is passed, we can simply copy over values
-			// since HitResult is also an instance of CurveLocation.
-			this._item = item._curve._path;
-			Base.each(item, function(value, key) {
-				this[key] = value;
-			}, this);
-		} else {
-			this._item = item;
-		}
+HitResult = Base.extend(/** @lends HitResult# */{
+	initialize: function(type, item, location) {
+		this.type = type;
+		this.item = item;
+		if (location)
+			this.location = location;
 	},
 
 	statics: {
@@ -46,29 +39,31 @@ HitResult = CurveLocation.extend(/** @lends HitResult# */{
 		getOptions: function(options) {
 			// TODO: Consier moving to HitResult / HitEvent?
 			return options && options._merged ? options : Base.merge({
+				// Type of item, for instanceof check: PathItem, TexItem, etc
+				type: Item,
+				// Tolerance
+				tolerance: 2,
 				// Hit the fill of items
 				fill: true,
 				// Hit the curves of path items, taking into account the stroke
 				// width.
 				stroke: true,
-				// Hit the part of segments that curves pass through
-				// (Segment#point)
-				// TODO: Shall we call this points?
+				// Hit the part of segments that curves pass through, excluding
+				// its segments (Segment#point)
 				segments: true,
-				// Only first or last segment hits on path (mutually exclusive
-				// with segments: true)
-				// TODO: Shall we calls this: endPoints?
-				ends: false,
 				// Hit the parts of segments that define the curvature
 				handles: true,
+				// Only first or last segment hits on path (mutually exclusive
+				// with segments: true)
+				ends: false,
+				// Hit test the center of the bounds
+				center: false,
+				// Hit test the corners and side-centers of the boudning box
+				bounds: false,
 				//  Hit items that are marked as guides
 				guides: false,
 				// Only hit selected objects
 				selected: false,
-				// Type of item, for instanceof check: PathItem, TexItem, etc
-				type: Item,
-				// Tolerance
-				tolerance: 2,
 				// Mark as merged, so next time Base.merge isn't called
 				_merged: true
 			}, options);
