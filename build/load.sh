ce6926afa2918f3ea0d72ca75ac43cccbc36ea02
Jürg Lehni <juerg@scratchdisk.com>
2011-07-27 22:00:31 +0100

    Move prepro.js to lib/and include() statements for test units to their own files inside tests/

--- build/load.sh
+++ build/load.sh
@@ -18,5 +18,5 @@
 # development of the library itself.
 
 echo "// Paper.js loader for development, as produced by the build/load.sh script
-document.write('<script type=\"text/javascript\" src=\"../../src/load/prepro.js\"></script>');
-document.write('<script type=\"text/javascript\" src=\"../../src/load/load.js\"></script>');" > ../dist/paper.js;
+document.write('<script type=\"text/javascript\" src=\"../../lib/prepro.js\"></script>');
+document.write('<script type=\"text/javascript\" src=\"../../src/load.js\"></script>');" > ../dist/paper.js;
