0e6aeac28c0b73ab5e6bd05ffeca1398b5858d95
Jürg Lehni <juerg@scratchdisk.com>
2011-08-01 08:53:28 +0100

    Fix HSLColor documentation.

--- src/color/Color.js
+++ src/color/Color.js
@@ -746,7 +746,7 @@ var HSLColor = this.HSLColor = Color.extend(/** @lends HSLColor# */{
 	 * var circle = new Path.Circle(new Point(80, 50), 30);
 	 *
 	 * // Create an HSLColor with a hue of 90 degrees, a saturation
-	 * // 100% and a lightness of 100%:
+	 * // 100% and a lightness of 50%:
 	 * circle.fillColor = new HSLColor(90, 1, 0.5);
 	 */
 
