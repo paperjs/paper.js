1baad430a62a75e11dab4dac2a29e26eca89ec94
Jürg Lehni <juerg@scratchdisk.com>
2011-11-10 19:16:34 +0100

    Rename RGBColor -> RgbColor, to avoid clash with native constructor, and follow same naming convention for HsbColor and HslColor, but keep references to old names for PaperScript scopes for now.

--- test/tests/Color.js
+++ test/tests/Color.js
@@ -19,69 +19,69 @@ module('Color');
 test('Set named color', function() {
 	var path = new Path();
 	path.fillColor = 'red';
-	compareRGBColors(path.fillColor, new RGBColor(1, 0, 0));
+	compareRgbColors(path.fillColor, new RgbColor(1, 0, 0));
 	equals(path.fillColor.toCssString(), 'rgba(255, 0, 0, 1)');
 });
 
 test('Set color to hex', function() {
 	var path = new Path();
 	path.fillColor = '#ff0000';
-	compareRGBColors(path.fillColor, new RGBColor(1, 0, 0));
+	compareRgbColors(path.fillColor, new RgbColor(1, 0, 0));
 	equals(path.fillColor.toCssString(), 'rgba(255, 0, 0, 1)');
 
 	var path = new Path();
 	path.fillColor = '#f00';
-	compareRGBColors(path.fillColor, new RGBColor(1, 0, 0));
+	compareRgbColors(path.fillColor, new RgbColor(1, 0, 0));
 	equals(path.fillColor.toCssString(), 'rgba(255, 0, 0, 1)');
 });
 
 test('Set color to object', function() {
 	var path = new Path();
 	path.fillColor = { red: 1, green: 0, blue: 1};
-	compareRGBColors(path.fillColor, new RGBColor(1, 0, 1));
+	compareRgbColors(path.fillColor, new RgbColor(1, 0, 1));
 	equals(path.fillColor.toCssString(), 'rgba(255, 0, 255, 1)');
 
 	var path = new Path();
 	path.fillColor = { gray: 0.2 };
-	compareRGBColors(path.fillColor, new RGBColor(0.8, 0.8, 0.8));
+	compareRgbColors(path.fillColor, new RgbColor(0.8, 0.8, 0.8));
 	equals(path.fillColor.toCssString(), 'rgba(204, 204, 204, 1)');
 });
 
 test('Set color to array', function() {
 	var path = new Path();
 	path.fillColor = [1, 0, 0];
-	compareRGBColors(path.fillColor, new RGBColor(1, 0, 0));
+	compareRgbColors(path.fillColor, new RgbColor(1, 0, 0));
 	equals(path.fillColor.toCssString(), 'rgba(255, 0, 0, 1)');
 });
 
 test('Creating colors', function() {
 
-	compareRGBColors(new RGBColor('#ff0000'), new RGBColor(1, 0, 0),
-			'RGBColor from hex code');
+	compareRgbColors(new RgbColor('#ff0000'), new RgbColor(1, 0, 0),
+			'RgbColor from hex code');
 
-	compareRGBColors(new RGBColor({ red: 1, green: 0, blue: 1}),
-			new RGBColor(1, 0, 1), 'RGBColor from rgb object literal');
+	compareRgbColors(new RgbColor({ red: 1, green: 0, blue: 1}),
+			new RgbColor(1, 0, 1), 'RgbColor from rgb object literal');
 
-	compareRGBColors(new RGBColor({ gray: 0.2 }),
-			new RGBColor(0.8, 0.8, 0.8), 'RGBColor from gray object literal');
+	compareRgbColors(new RgbColor({ gray: 0.2 }),
+			new RgbColor(0.8, 0.8, 0.8), 'RgbColor from gray object literal');
 
-	compareRGBColors(new RGBColor({ hue: 0, saturation: 1, brightness: 1}),
-			new RGBColor(1, 0, 0), 'RGBColor from hsb object literal');
+	compareRgbColors(new RgbColor({ hue: 0, saturation: 1, brightness: 1}),
+			new RgbColor(1, 0, 0), 'RgbColor from hsb object literal');
 
-	compareRGBColors(new RGBColor([1, 0, 0]), new RGBColor(1, 0, 0),
-			'RGBColor from array');
+	compareRgbColors(new RgbColor([1, 0, 0]), new RgbColor(1, 0, 0),
+			'RgbColor from array');
 
-	compareHSBColors(new HSBColor('#000000'), new HSBColor(0, 0, 0),
-			'HSBColor from hex code');
+	compareHsbColors(new HsbColor('#000000'), new HsbColor(0, 0, 0),
+			'HsbColor from hex code');
 
-	compareHSBColors(new HSBColor({ red: 1, green: 0, blue: 0}),
-			new HSBColor(0, 1, 1), 'HSBColor from rgb object literal');
+	compareHsbColors(new HsbColor({ red: 1, green: 0, blue: 0}),
+			new HsbColor(0, 1, 1), 'HsbColor from rgb object literal');
 
-	compareHSBColors(new HSBColor({ gray: 0.8 }),
-			new HSBColor(0, 0, 0.2), 'RGBColor from gray object literal');
+	compareHsbColors(new HsbColor({ gray: 0.8 }),
+			new HsbColor(0, 0, 0.2), 'RgbColor from gray object literal');
 
-	compareHSBColors(new HSBColor([1, 0, 0]), new HSBColor(1, 0, 0),
-			'HSBColor from array');
+	compareHsbColors(new HsbColor([1, 0, 0]), new HsbColor(1, 0, 0),
+			'HsbColor from array');
 
 	compareGrayColors(new GrayColor('#000000'), new GrayColor(1),
 			'GrayColor from hex code');
@@ -102,26 +102,26 @@ test('Creating colors', function() {
 			'GrayColor from array');
 });
 
-test('Get gray from RGBColor', function() {
-	var color = new RGBColor(1, 0.5, 0.2);
+test('Get gray from RgbColor', function() {
+	var color = new RgbColor(1, 0.5, 0.2);
 	compareNumbers(color.gray, 0.38458251953125);
 
-	var color = new RGBColor(0.5, 0.2, 0.1);
+	var color = new RgbColor(0.5, 0.2, 0.1);
 	compareNumbers(color.gray, 0.72137451171875);
 });
 
-test('Get gray from HSBColor', function() {
-	var color = new HSBColor(0, 0, 0.2);
+test('Get gray from HsbColor', function() {
+	var color = new HsbColor(0, 0, 0.2);
 	compareNumbers(color.gray, 0.8);
 });
 
-test('Get red from HSBColor', function() {
-	var color = new HSBColor(0, 1, 1);
+test('Get red from HsbColor', function() {
+	var color = new HsbColor(0, 1, 1);
 	compareNumbers(color.red, 1);
 });
 
-test('Get hue from RGBColor', function() {
-	var color = new RGBColor(1, 0, 0);
+test('Get hue from RgbColor', function() {
+	var color = new RgbColor(1, 0, 0);
 	compareNumbers(color.hue, 0);
 	compareNumbers(color.saturation, 1);
 });
@@ -139,56 +139,56 @@ test('Gray Color', function() {
 });
 
 test('Converting Colors', function() {
-	var rgbColor = new RGBColor(1, 0.5, 0.2);
+	var rgbColor = new RgbColor(1, 0.5, 0.2);
 	compareNumbers(new GrayColor(rgbColor).gray, 0.38299560546875);
 
 	var grayColor = new GrayColor(0.2);
-	var rgbColor = new RGBColor(grayColor);
-	compareRGBColors(rgbColor, [ 0.8, 0.8, 0.8, 1]);
+	var rgbColor = new RgbColor(grayColor);
+	compareRgbColors(rgbColor, [ 0.8, 0.8, 0.8, 1]);
 
-	var hsbColor = new HSBColor(grayColor);
-	compareHSBColors(hsbColor, [ 0, 0, 0.8, 1]);
+	var hsbColor = new HsbColor(grayColor);
+	compareHsbColors(hsbColor, [ 0, 0, 0.8, 1]);
 
-	var rgbColor = new RGBColor(1, 0, 0);
-	compareHSBColors(new HSBColor(rgbColor), [0, 1, 1, 1]);
+	var rgbColor = new RgbColor(1, 0, 0);
+	compareHsbColors(new HsbColor(rgbColor), [0, 1, 1, 1]);
 });
 
-test('Setting RGBColor#gray', function() {
-	var color = new RGBColor(1, 0.5, 0.2);
+test('Setting RgbColor#gray', function() {
+	var color = new RgbColor(1, 0.5, 0.2);
 	color.gray = 0.1;
-	compareRGBColors(color, [ 0.9, 0.9, 0.9, 1]);
+	compareRgbColors(color, [ 0.9, 0.9, 0.9, 1]);
 });
 
-test('Setting HSBColor#red', function() {
-	var color = new HSBColor(180, 0, 0);
+test('Setting HsbColor#red', function() {
+	var color = new HsbColor(180, 0, 0);
 	color.red = 1;
-	compareHSBColors(color, [0, 1, 1, 1]);
+	compareHsbColors(color, [0, 1, 1, 1]);
 });
 
-test('Setting HSBColor#gray', function() {
-	var color = new HSBColor(180, 0, 0);
+test('Setting HsbColor#gray', function() {
+	var color = new HsbColor(180, 0, 0);
 	color.gray = 0.5;
-	compareHSBColors(color, [0, 0, 0.5, 1]);
+	compareHsbColors(color, [0, 0, 0.5, 1]);
 });
 
 test('Color.read(channels)', function() {
 	var color = Color.read([0, 0, 1]);
-	compareRGBColors(color, [0, 0, 1, 1]);
+	compareRgbColors(color, [0, 0, 1, 1]);
 });
 
 test('Cloning colors', function() {
-	var color = new RGBColor(0, 0, 0);
+	var color = new RgbColor(0, 0, 0);
 	equals(function() {
 		return color.clone() != color;
 	}, true);
 
 	equals(function() {
-		return new RGBColor(color) != color;
+		return new RgbColor(color) != color;
 	}, true);
 });
 
 test('Color#convert', function() {
-	var color = new RGBColor(0, 0, 0);
+	var color = new RgbColor(0, 0, 0);
 	var converted = color.convert('rgb');
 	equals(function() {
 		return converted !== color;
@@ -200,6 +200,6 @@ test('Color#convert', function() {
 
 test('Saturation from black rgb', function() {
 	equals(function() {
-		return new RGBColor(0, 0, 0).saturation == 0;
+		return new RgbColor(0, 0, 0).saturation == 0;
 	}, true);
 });
