777d72e5b101fdd8bb450e26d4bf7822c991779e
Jonathan Puckey <me@jonathanpuckey.com>
2012-03-13 14:54:23 +0100

    Add failing tests for Item#moveAbove(item) and Item#moveBelow(item)

--- test/tests/Item_Order.js
+++ test/tests/Item_Order.js
@@ -57,3 +57,44 @@ test('Item Order', function() {
 		return group.isBelow(circle);
 	}, false);
 });
+
+test('Item#moveAbove(item) / Item#moveBelow(item)', function() {
+	var item0, item1, item2;
+	var testMove = function(command, indexes) {
+		paper.project.activeLayer.remove();
+		new Layer();
+		item0 = new Group();
+		item1 = new Group();
+		item2 = new Group();
+		command();
+		equals(function() {
+			return item0.index;
+		}, indexes[0]);
+		equals(function() {
+			return item1.index;
+		}, indexes[1]);
+		equals(function() {
+			return item2.index;
+		}, indexes[2]);
+	}
+
+	testMove(function() { item0.moveBelow(item0) }, [0,1,2]);
+	testMove(function() { item0.moveBelow(item1) }, [0,1,2]);
+	testMove(function() { item0.moveBelow(item2) }, [1,0,2]);
+	testMove(function() { item1.moveBelow(item0) }, [1,0,2]);
+	testMove(function() { item1.moveBelow(item1) }, [0,1,2]);
+	testMove(function() { item1.moveBelow(item2) }, [0,1,2]);
+	testMove(function() { item2.moveBelow(item0) }, [2,0,1]);
+	testMove(function() { item2.moveBelow(item1) }, [0,2,1]);
+	testMove(function() { item2.moveBelow(item2) }, [0,1,2]);
+ 
+	testMove(function() { item0.moveAbove(item0) }, [0,1,2]);
+	testMove(function() { item0.moveAbove(item1) }, [1,0,2]);
+	testMove(function() { item0.moveAbove(item2) }, [1,2,0]);
+	testMove(function() { item1.moveAbove(item0) }, [0,1,2]);
+	testMove(function() { item1.moveAbove(item1) }, [0,1,2]);
+	testMove(function() { item1.moveAbove(item2) }, [0,2,1]);
+	testMove(function() { item2.moveAbove(item0) }, [0,2,1]);
+	testMove(function() { item2.moveAbove(item1) }, [0,1,2]);
+	testMove(function() { item2.moveAbove(item2) }, [0,1,2]);
+});
