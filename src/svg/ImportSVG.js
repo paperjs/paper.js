24f0535bb6444ffd701680375bf254906cc9c7f3
Justin Ridgewell <justin@ridgewell.name>
2012-09-15 19:59:53 -0400

    Convert tabs to 4 spaces

--- src/svg/ImportSVG.js
+++ src/svg/ImportSVG.js
@@ -6,49 +6,49 @@
 
 var ImportSVG = function()
 {
-	//initialize
-	function initialize()
-	{
+    //initialize
+    function initialize()
+    {
 
-	};
+    };
 
-	/**
-	 *
-	 * Takes the svg dom obj and parses the data
-	 * to create a layer with groups (if needed) with
-	 * items inside. Should support nested groups.
-	 *
-	 * takes in a svg object (xml dom)
-	 * returns Paper.js Layer
-	 */
+    /**
+     *
+     * Takes the svg dom obj and parses the data
+     * to create a layer with groups (if needed) with
+     * items inside. Should support nested groups.
+     *
+     * takes in a svg object (xml dom)
+     * returns Paper.js Layer
+     */
     this.importSVG = function(svg)
     {
-    	return layer;
+        return layer;
     };
 
-	/**
-	 * Creates a Paper.js Group by parsing
-	 * a specific svg g node
-	 *
-	 * takes in a svg object (xml dom)
-	 * returns Paper.js Group
-	 */
+    /**
+     * Creates a Paper.js Group by parsing
+     * a specific svg g node
+     *
+     * takes in a svg object (xml dom)
+     * returns Paper.js Group
+     */
     function importGroup(svg)
     {
-    	return group;
+        return group;
     };
 
-	/**
-	 * Creates a Paper.js Path by parsing
-	 * a specific svg node (rect, path, circle, polygon, etc)
-	 * and creating the right path object based on the svg type.
-	 *
-	 * takes in a svg object (xml dom)
-	 * returns Paper.js Group
-	 */
+    /**
+     * Creates a Paper.js Path by parsing
+     * a specific svg node (rect, path, circle, polygon, etc)
+     * and creating the right path object based on the svg type.
+     *
+     * takes in a svg object (xml dom)
+     * returns Paper.js Group
+     */
     function importPath(svg)
     {
-    	return path;
+        return path;
     };
 
     initialize(); // calls the init function after class is loaded
