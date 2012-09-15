0f329db4df7ba3b0da33a298da6b2ecd7f10532f
Justin Ridgewell <justin@ridgewell.name>
2012-09-15 19:58:44 -0400

    Strip trailing whitespace

--- src/svg/ImportSVG.js
+++ src/svg/ImportSVG.js
@@ -1,7 +1,7 @@
 /**
  *  Imports svg into items with groups
  *  Stetson Alpha - Paper.js
- *  
+ *
  */
 
 var ImportSVG = function()
@@ -9,27 +9,27 @@ var ImportSVG = function()
 	//initialize
 	function initialize()
 	{
-		
+
 	};
-	
+
 	/**
-	 * 
+	 *
 	 * Takes the svg dom obj and parses the data
 	 * to create a layer with groups (if needed) with
 	 * items inside. Should support nested groups.
-	 * 
+	 *
 	 * takes in a svg object (xml dom)
-	 * returns Paper.js Layer 
+	 * returns Paper.js Layer
 	 */
     this.importSVG = function(svg)
     {
     	return layer;
     };
-	
+
 	/**
 	 * Creates a Paper.js Group by parsing
 	 * a specific svg g node
-	 * 
+	 *
 	 * takes in a svg object (xml dom)
 	 * returns Paper.js Group
 	 */
@@ -37,12 +37,12 @@ var ImportSVG = function()
     {
     	return group;
     };
-    
+
 	/**
 	 * Creates a Paper.js Path by parsing
 	 * a specific svg node (rect, path, circle, polygon, etc)
 	 * and creating the right path object based on the svg type.
-	 * 
+	 *
 	 * takes in a svg object (xml dom)
 	 * returns Paper.js Group
 	 */
@@ -50,6 +50,6 @@ var ImportSVG = function()
     {
     	return path;
     };
-	
+
     initialize(); // calls the init function after class is loaded
 };
