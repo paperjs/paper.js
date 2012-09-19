5f550a9f8a3f731d76f6c69b4b482f3d42fc843b
AWagenheim <awagenhe@stetson.edu>
2012-09-18 22:35:48 -0400

    Added ImportSVG.js Stub

---
+++ test/tests/ImportSVG.js
@@ -0,0 +1,21 @@
+/*
+* Paper.js
+*
+* This file is part of Paper.js, a JavaScript Vector Graphics Library,
+* based on Scriptographer.org and designed to be largely API compatible.
+* http://paperjs.org/
+* http://scriptographer.org/
+*
+* Copyright (c) 2011, Juerg Lehni & Jonathan Puckey
+* http://lehni.org/ & http://jonathanpuckey.com/
+*
+* Distributed under the MIT license. See LICENSE file for details.
+*
+* All rights reserved.
+*/
+
+module('ImportSVG');
+
+/**
+*TODO: Create SVGs utilizing ImportSVG methods
+*/
