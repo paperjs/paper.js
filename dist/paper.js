19c4449165fc372ddd9f2036f1a61a55697b809b
skierons <skierons@stetson.edu>
2012-09-30 19:09:58 -0300

    Update dist/paper.js

---
+++ dist/paper.js
@@ -0,0 +1,3 @@
+// Paper.js loader for development, as produced by the build/load.sh script
+document.write('<script type="text/javascript" src="../../lib/prepro.js"></script>');
+document.write('<script type="text/javascript" src="../../src/load.js"></script>');
