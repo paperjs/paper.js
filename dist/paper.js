f6d487f7db887105faac05f5f193cabab6001bf9
Andrew <jaroles@stetson.edu>
2012-09-30 20:56:59 -0400

    Setting up for merge

--- dist/paper.js
+++
@@ -1,3 +0,0 @@
-// Paper.js loader for development, as produced by the build/load.sh script
-document.write('<script type="text/javascript" src="../../lib/prepro.js"></script>');
-document.write('<script type="text/javascript" src="../../src/load.js"></script>');
