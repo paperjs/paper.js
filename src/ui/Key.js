709c6cc8b8b6b91bee09bcd2808f5cca3924eb6b
Jürg Lehni <juerg@scratchdisk.com>
2012-10-30 11:43:06 -0700

    Do not implicitly use getTool() getter since it creates tools when none exists.

--- src/ui/Key.js
+++ src/ui/Key.js
@@ -71,7 +71,7 @@ var Key = this.Key = new function() {
 			type = down ? 'keydown' : 'keyup',
 			view = View._focused,
 			scope = view && view.isVisible() && view._scope,
-			tool = scope && scope.tool;
+			tool = scope && scope._tool;
 		keyMap[key] = down;
 		if (tool && tool.responds(type)) {
 			// Call the onKeyDown or onKeyUp handler if present
