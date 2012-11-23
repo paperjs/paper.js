40c3b643915d2c8a1b96103dfcdadd4181f4adcc
Jonathan Puckey <me@jonathanpuckey.com>
2012-11-23 21:49:07 +0100

    Implement PointText#drawSelected. Closes #127.

--- src/text/PointText.js
+++ src/text/PointText.js
@@ -84,6 +84,13 @@ var PointText = this.PointText = TextItem.extend(/** @lends PointText# */{
 				ctx.strokeText(line, 0, 0);
 			ctx.translate(0, leading);
 		}
+	},
+
+	// TODO: Should we be drawing the selection like this? Perhaps a small
+	// rectangle for the starting point of the text and a colored line on
+	// the baseline for its width?
+	drawSelected: function(ctx, matrix) {
+		Item.drawSelectedBounds(this._getBounds(), ctx, matrix);
 	}
 }, new function() {
 	var context = null;
