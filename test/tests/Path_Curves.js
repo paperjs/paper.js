bafd92f63c6e6aab34e76f6ae8f8ebdef1416895
Jürg Lehni <juerg@scratchdisk.com>
2012-12-31 21:56:17 +0100

    Change path.curves tests to include test for inserting first segment too.

--- test/tests/Path_Curves.js
+++ test/tests/Path_Curves.js
@@ -19,12 +19,12 @@ module('Path Curves');
 test('path.curves Synchronisation', function() {
 	var path = new Path();
 
-	path.add(new Point(0, 100));
-	equals(path.segments.toString(), "{ point: { x: 0, y: 100 } }", "path.segments: path.add(new Point(0, 100));");
-	equals(path.curves.toString(), "", "path.curves: path.add(new Point(0, 100));");
 	path.add(new Point(100, 100));
-	equals(path.segments.toString(), "{ point: { x: 0, y: 100 } },{ point: { x: 100, y: 100 } }", "path.segments: path.add(new Point(100, 100));");
-	equals(path.curves.toString(), "{ point1: { x: 0, y: 100 }, point2: { x: 100, y: 100 } }", "path.curves: path.add(new Point(100, 100));");
+	equals(path.segments.toString(), "{ point: { x: 100, y: 100 } }", "path.segments: path.add(new Point(100, 100));");
+	equals(path.curves.toString(), "", "path.curves: path.add(new Point(100, 100));");
+	path.insert(0, new Point(0, 100));
+	equals(path.segments.toString(), "{ point: { x: 0, y: 100 } },{ point: { x: 100, y: 100 } }", "path.segments: path.insert(0, new Point(0, 100));");
+	equals(path.curves.toString(), "{ point1: { x: 0, y: 100 }, point2: { x: 100, y: 100 } }", "path.curves: path.insert(0, new Point(0, 100));");
 	path.insert(1, {point:[50, 0], handleIn:[-25, 0], handleOut:[25, 0]});
 	equals(path.segments.toString(), "{ point: { x: 0, y: 100 } },{ point: { x: 50, y: 0 }, handleIn: { x: -25, y: 0 }, handleOut: { x: 25, y: 0 } },{ point: { x: 100, y: 100 } }", "path.segments: path.insert(1, {point:[50, 0], handleIn:[-25, 0], handleOut:[25, 0]});");
 	equals(path.curves.toString(), "{ point1: { x: 0, y: 100 }, handle2: { x: -25, y: 0 }, point2: { x: 50, y: 0 } },{ point1: { x: 50, y: 0 }, handle1: { x: 25, y: 0 }, point2: { x: 100, y: 100 } }", "path.curves: path.insert(1, {point:[50, 0], handleIn:[-25, 0], handleOut:[25, 0]});");
