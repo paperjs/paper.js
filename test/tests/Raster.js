9865d7bdd52cb4220f57143314f438600bb9fb12
Jonathan Puckey <me@jonathanpuckey.com>
2013-03-17 18:13:55 +0100

    Add raster constructor tests, add async test helper function and test image.

--- test/tests/Raster.js
+++ test/tests/Raster.js
@@ -22,3 +22,48 @@ test('Create a raster without a source and set its size', function() {
 	raster.size = [640, 480];
 	equals(raster.size.toString(), new Size(640, 480).toString(), true);
 });
+
+asyncTest('Create a raster from a url', function(callback) {
+	var raster = new Raster('resources/paper-js.gif');
+	raster.onLoad = function() {
+		equals(raster.size.toString(), new Size(146, 146).toString(), true);
+		callback();
+	};
+});
+
+asyncTest('Create a raster from a dom image', function(callback) {
+	var img = document.createElement('img');
+	img.src = 'resources/paper-js.gif';
+	document.body.appendChild(img);
+	DomEvent.add(img, {
+		load: function() {
+			var raster = new Raster(img);
+			equals(raster.size.toString(), new Size(146, 146).toString(), true);
+			document.body.removeChild(img);
+			callback();
+		}
+	});
+});
+
+
+test('Create a raster from a canvas', function(callback) {
+	var canvas = CanvasProvider.getCanvas(30, 20);
+	var raster = new Raster(canvas);
+	equals(raster.size.toString(), new Size(30, 20).toString(), true);
+	CanvasProvider.release(canvas);
+});
+
+asyncTest('Create a raster from a dom id', function(callback) {
+	var img = document.createElement('img');
+	img.src = 'resources/paper-js.gif';
+	img.id = 'testimage';
+	document.body.appendChild(img);
+	DomEvent.add(img, {
+		load: function() {
+			var raster = new Raster('testimage');
+			equals(raster.size.toString(), new Size(146, 146).toString(), true);
+			document.body.removeChild(img);
+			callback();
+		}
+	});
+});
