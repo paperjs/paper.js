3d0843ec728ec227d80e5b5c924a92971cc0fa43
Jürg Lehni <juerg@scratchdisk.com>
2013-03-19 16:23:36 -0700

    Use same precision in Path#getPathData() as everywhere else.

--- src/path/Path.js
+++ src/path/Path.js
@@ -206,7 +206,7 @@ var Path = this.Path = PathItem.extend(/** @lends Path# */{
 		var segments = this._segments,
 			style = this._style,
 			format = Format.point,
-			precision = arguments[0] || 3,
+			precision = arguments[0],
 			parts = [];
 
 		// TODO: Add support for H/V and/or relative commands, where appropriate
