2f0bcfabd4ed0df2c251a05018a7a9a737862a9e
hkrish <hari.exeption@gmail.com>
2013-05-04 02:17:33 +0200

    Subdivision doesn't work! rewrite!

--- fatline/Intersect.js
+++ fatline/Intersect.js
@@ -21,113 +21,136 @@ function _clipFatLine( v1, v2, t1, t2, u1, u2, tdiff, udiff, tvalue, curve1, cur
     if( t1 >= t2 - _tolerence && t1 <= t2 + _tolerence && u1 >= u2 - _tolerence && u1 <= u2 + _tolerence ){
         var loc = tvalue ? new CurveLocation( curve2, t1, null, curve1 ) :
          new CurveLocation( curve1, u1, null, curve2 );
+         // console.log( t1, t2, u1, u2 )
         locations.push( loc );
-        return;
-    }
-    p0x = v1[0]; p0y = v1[1];
-    p3x = v1[6]; p3y = v1[7];
-    p1x = v1[2]; p1y = v1[3];
-    p2x = v1[4]; p2y = v1[5];
-    q0x = v2[0]; q0y = v2[1];
-    q3x = v2[6]; q3y = v2[7];
-    q1x = v2[2]; q1y = v2[3];
-    q2x = v2[4]; q2y = v2[5];
-    // Calculate L
-    var d1 = _getSignedDist( p0x, p0y, p3x, p3y, p1x, p1y );
-    var d2 = _getSignedDist( p0x, p0y, p3x, p3y, p2x, p2y );
-    var dmin, dmax;
-    if( d1 * d2 > 0){
-        // 3/4 * min{0, d1, d2}
-        dmin = 0.75 * Math.min( 0, d1, d2 );
-        dmax = 0.75 * Math.max( 0, d1, d2 );
+        // return;
     } else {
-        // 4/9 * min{0, d1, d2}
-        dmin = 4 * Math.min( 0, d1, d2 ) / 9.0;
-        dmax = 4 * Math.max( 0, d1, d2 ) / 9.0;
-    }
-    // The convex hull for the non-parametric bezier curve D(ti, di(t))
-    var dq0 = _getSignedDist( p0x, p0y, p3x, p3y, q0x, q0y );
-    var dq1 = _getSignedDist( p0x, p0y, p3x, p3y, q1x, q1y );
-    var dq2 = _getSignedDist( p0x, p0y, p3x, p3y, q2x, q2y );
-    var dq3 = _getSignedDist( p0x, p0y, p3x, p3y, q3x, q3y );
-
-    var mindist = Math.min( dq0, dq3 );
-    var maxdist = Math.max( dq0, dq3 );
-    // If the fatlines don't overlap, we have no intersections!
-    if( dmin > maxdist || dmax < mindist ){
-        return;
-    }
-    // Ideally we need to calculate the convex hull for D(ti, di(t))
-    // here we are just checking against all possibilities
-    var Dt = [
-        [ 0.0, dq0, 0.3333333333333333, dq1 ],
-        [ 0.3333333333333333, dq1, 0.6666666666666666, dq2 ],
-        [ 0.6666666666666666, dq2, 1.0, dq3 ],
-        [ 1.0, dq3, 0.0, dq0 ],
-        [ 0.0, dq0, 0.6666666666666666, dq2 ],
-        [ 1.0, dq3, 0.3333333333333333, dq1 ]
-    ];
-    // Now we clip the convex hulls for D(ti, di(t)) with dmin and dmax
-    // for the coorresponding t values
-    var tmindmin = Infinity, tmaxdmin = -Infinity,
-    tmindmax = Infinity, tmaxdmax = -Infinity, ixd, ixdx, i;
-    var dmina = [0, dmin, 2, dmin];
-    var dmaxa = [0, dmax, 2, dmax];
-    for (i = 0; i < 6; i++) {
-        var Dtl = Dt[i];
-        // ixd = Dtl.intersect( vecdmin );
-        ixd = _intersectLines( Dtl, dmina);
-        if( ixd ){
-            ixdx = ixd[0];
-            tmindmin = ( ixdx < tmindmin )? ixdx : tmindmin;
-            tmaxdmin = ( ixdx > tmaxdmin )? ixdx : tmaxdmin;
+        p0x = v1[0]; p0y = v1[1];
+        p3x = v1[6]; p3y = v1[7];
+        p1x = v1[2]; p1y = v1[3];
+        p2x = v1[4]; p2y = v1[5];
+        q0x = v2[0]; q0y = v2[1];
+        q3x = v2[6]; q3y = v2[7];
+        q1x = v2[2]; q1y = v2[3];
+        q2x = v2[4]; q2y = v2[5];
+        // Calculate L
+        var d1 = _getSignedDist( p0x, p0y, p3x, p3y, p1x, p1y );
+        var d2 = _getSignedDist( p0x, p0y, p3x, p3y, p2x, p2y );
+        var dmin, dmax;
+        if( d1 * d2 > 0){
+            // 3/4 * min{0, d1, d2}
+            dmin = 0.75 * Math.min( 0, d1, d2 );
+            dmax = 0.75 * Math.max( 0, d1, d2 );
+        } else {
+            // 4/9 * min{0, d1, d2}
+            dmin = 4 * Math.min( 0, d1, d2 ) / 9.0;
+            dmax = 4 * Math.max( 0, d1, d2 ) / 9.0;
         }
-        // ixd = Dtl.intersect( vecdmax );
-        ixd = _intersectLines( Dtl, dmaxa);
-        if( ixd ){
-            ixdx = ixd[0];
-            tmindmax = ( ixdx < tmindmax )? ixdx : tmindmax;
-            tmaxdmax = ( ixdx > tmaxdmax )? ixdx : tmaxdmax;
+        // The convex hull for the non-parametric bezier curve D(ti, di(t))
+        var dq0 = _getSignedDist( p0x, p0y, p3x, p3y, q0x, q0y );
+        var dq1 = _getSignedDist( p0x, p0y, p3x, p3y, q1x, q1y );
+        var dq2 = _getSignedDist( p0x, p0y, p3x, p3y, q2x, q2y );
+        var dq3 = _getSignedDist( p0x, p0y, p3x, p3y, q3x, q3y );
+
+        var mindist = Math.min( dq0, dq1, dq2, dq3 );
+        var maxdist = Math.max( dq0, dq1, dq2, dq3 );
+        // If the fatlines don't overlap, we have no intersections!
+        if( dmin > maxdist || dmax < mindist ){
+            return;
         }
-    }
-    // if dmin doesnot intersect with the convexhull, reset it to 0
-    tmindmin = ( tmindmin === Infinity )? 0 : tmindmin;
-    tmaxdmin = ( tmaxdmin === -Infinity )? 0 : tmaxdmin;
-    // if dmax doesnot intersect with the convexhull, reset it to 1
-    tmindmax = ( tmindmax === Infinity )? 1 : tmindmax;
-    tmaxdmax = ( tmaxdmax === -Infinity )? 1 : tmaxdmax;
-    var tmin = Math.min( tmindmin, tmaxdmin, tmindmax, tmaxdmax );
-    var tmax = Math.max( tmindmin, tmaxdmin, tmindmax, tmaxdmax);
-
-    // if( count === 1 ){
-    //   console.log( dmin, dmax, tmin, tmax, " - ", tmindmin, tmaxdmin, tmindmax, tmaxdmax )
-    //   plotD_vs_t( 250, 110, Dt, dmin, dmax, tmin, tmax, 1, tvalue );
-    // }
-
-    // return;
-
-    // We need to toggle clipping both curves alternatively
-    // tvalue indicates whether to compare t or u for testing for convergence
-    var nuV2 = Curve.getPart( v2, tmin, tmax );
-    var convRate;
-    if( tvalue ){
-        nuT1 = t1 + tmin * ( t2 - t1 );
-        nuT2 = t1 + tmax * ( t2 - t1 );
-        // Test the convergence rate
-        // if the clipping fails to converge atleast 20%,
-        // subdivide the longest curve.
-        convRate = (tdiff - tmax + tmin ) / tdiff;
-        if( convRate <= 0.2) {
-
+        // Ideally we need to calculate the convex hull for D(ti, di(t))
+        // here we are just checking against all possibilities and sorting them
+        // TODO: implement simple polygon convexhull method.
+        var Dt = [
+            [ 0.0, dq0, 0.3333333333333333, dq1 ],
+            [ 0.3333333333333333, dq1, 0.6666666666666666, dq2 ],
+            [ 0.6666666666666666, dq2, 1.0, dq3 ],
+            [ 1.0, dq3, 0.0, dq0 ],
+            [ 0.0, dq0, 0.6666666666666666, dq2 ],
+            [ 1.0, dq3, 0.3333333333333333, dq1 ]
+        ];
+        // Now we clip the convex hulls for D(ti, di(t)) with dmin and dmax
+        // for the coorresponding t values
+        var tmindmin = Infinity, tmaxdmin = -Infinity,
+        tmindmax = Infinity, tmaxdmax = -Infinity, ixd, ixdx, i;
+        var dmina = [0, dmin, 2, dmin];
+        var dmaxa = [0, dmax, 2, dmax];
+        for (i = 0; i < 6; i++) {
+            var Dtl = Dt[i];
+            // ixd = Dtl.intersect( vecdmin );
+            ixd = _intersectLines( Dtl, dmina);
+            if( ixd ){
+                ixdx = ixd[0];
+                tmindmin = ( ixdx < tmindmin )? ixdx : tmindmin;
+                tmaxdmin = ( ixdx > tmaxdmin )? ixdx : tmaxdmin;
+            }
+            // ixd = Dtl.intersect( vecdmax );
+            ixd = _intersectLines( Dtl, dmaxa);
+            if( ixd ){
+                ixdx = ixd[0];
+                tmindmax = ( ixdx < tmindmax )? ixdx : tmindmax;
+                tmaxdmax = ( ixdx > tmaxdmax )? ixdx : tmaxdmax;
+            }
         }
-        _clipFatLine( nuV2, v1, nuT1, nuT2, u1, u2, (tmax - tmin), udiff, !tvalue, curve1, curve2, locations, count );
-    } else {
-        nuU1 = u1 + tmin * ( u2 - u1 );
-        nuU2 = u1 + tmax * ( u2 - u1 );
+        // if dmin doesnot intersect with the convexhull, reset it to 0
+        tmindmin = ( tmindmin === Infinity )? 0 : tmindmin;
+        tmaxdmin = ( tmaxdmin === -Infinity )? 0 : tmaxdmin;
+        // if dmax doesnot intersect with the convexhull, reset it to 1
+        tmindmax = ( tmindmax === Infinity )? 1 : tmindmax;
+        tmaxdmax = ( tmaxdmax === -Infinity )? 1 : tmaxdmax;
+        var tmin = Math.min( tmindmin, tmaxdmin, tmindmax, tmaxdmax );
+        var tmax = Math.max( tmindmin, tmaxdmin, tmindmax, tmaxdmax);
 
-        convRate = ( udiff - tmax + tmin ) / udiff;
+        if( count === 1 ){
+          // console.log( dmin, dmax, tmin, tmax, " - ", tmindmin, tmaxdmin, tmindmax, tmaxdmax )
+          plotD_vs_t( 250, 110, Dt, dmin, dmax, tmin, tmax, 1, tvalue );
+                // markPoint( curve1.getPoint(0.5452178926340512), " " );
+                // markPoint( curve1.getPoint(0.9462539004114424), " " );
+        // return;
+        }
 
-        _clipFatLine( nuV2, v1, t1, t2, nuU1, nuU2 , tdiff, (tmax - tmin), !tvalue, curve1, curve2, locations, count );
+
+        // We need to toggle clipping both curves alternatively
+        // tvalue indicates whether to compare t or u for testing for convergence
+        var nuV2 = Curve.getPart( v2, tmin, tmax );
+        var convRate, parts;
+        if( tvalue ){
+            nuT1 = t1 + tmin * ( t2 - t1 );
+            nuT2 = t1 + tmax * ( t2 - t1 );
+            // Test the convergence rate
+            // if the clipping fails to converge atleast 20%,
+            // subdivide the longest curve.
+            convRate = (tdiff - tmax + tmin ) / tdiff;
+            console.log( 'convergence rate for t = ' + convRate + "%" );
+            if( convRate <= 0.2) {
+                // subdivide the curve and try again
+                parts = Curve.subdivide( v1 );
+                nuTHalf = (u2 + u1) / 2.0;
+                // _clipFatLine( v2, parts[0], u1, nuTHalf, t1, t2, udiff, (nuTHalf - u1 )/(u2 - u1), tvalue, curve2, curve1, locations, count );
+                // _clipFatLine( v2, parts[1], nuTHalf, u2, t1, t2, udiff, (u2 - nuTHalf )/(u2 - u1), tvalue, curve2, curve1, locations, count );
+                _clipFatLine( parts[0], v2, t1, t2, u1, nuTHalf, (nuTHalf - u1 )/(u2 - u1), udiff, !tvalue, curve1, curve2, locations, count );
+                _clipFatLine( parts[1], v2, t1, t2, nuTHalf, u2, (u2 - nuTHalf )/(u2 - u1), udiff, !tvalue, curve1, curve2, locations, count );
+            } else {
+                _clipFatLine( nuV2, v1, nuT1, nuT2, u1, u2, (tmax - tmin), udiff, !tvalue, curve1, curve2, locations, count );
+            }
+        } else {
+            nuU1 = u1 + tmin * ( u2 - u1 );
+            nuU2 = u1 + tmax * ( u2 - u1 );
+
+            convRate = ( udiff - tmax + tmin ) / udiff;
+            console.log( 'convergence rate for u = ' + convRate + "%" );
+            if( convRate <= 0.2) {
+                // subdivide the curve and try again
+                parts = Curve.subdivide( v2 );
+                nuTHalf = (t2 + t1) / 2.0;
+                // _clipFatLine( v2, parts[0], u1, nuTHalf, t1, t2, udiff, (nuTHalf - u1 )/(u2 - u1), tvalue, curve2, curve1, locations, count );
+                // _clipFatLine( v2, parts[1], nuTHalf, u2, t1, t2, udiff, (u2 - nuTHalf )/(u2 - u1), tvalue, curve2, curve1, locations, count );
+                _clipFatLine( parts[0], v1, t1, nuTHalf, u1, u2, tdiff, (nuTHalf - t1 )/(t2 - t1), !tvalue, curve1, curve2, locations, count );
+                _clipFatLine( parts[1], v1, nuTHalf, t2, u1, u2, tdiff, (t2 - nuTHalf )/(t2 - 1), !tvalue, curve1, curve2, locations, count );
+            } else {
+                _clipFatLine( nuV2, v1, t1, t2, nuU1, nuU2 , tdiff, (tmax - tmin), !tvalue, curve1, curve2, locations, count );
+            }
+        }
     }
 }
 
