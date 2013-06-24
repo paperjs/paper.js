1279e8017dd2619e83803bf7390a56dfcc1b015b
Jürg Lehni <juerg@scratchdisk.com>
2013-06-24 09:37:49 -0700

    Remove unnecessary minification switches.

--- build/minify.sh
+++ build/minify.sh
@@ -12,5 +12,5 @@
 
 # We need to keep dead_code around for now, since the very odd JavaScriptCore
 # scope bug fix (nop().nop()) requires it.
-uglifyjs ../dist/paper.js -o ../dist/paper-min.js -c unsafe=true,unused=false,dead_code=false,hoist_funs=false -m -b ascii_only=true,beautify=false --comments /^!/
-uglifyjs ../dist/paper-core.js -o ../dist/paper-core-min.js -c unsafe=true,unused=false,dead_code=false,hoist_funs=false -m --comments /^!/
+uglifyjs ../dist/paper.js -o ../dist/paper-min.js -c unsafe=true,unused=false -m -b ascii_only=true,beautify=false --comments /^!/
+uglifyjs ../dist/paper-core.js -o ../dist/paper-core-min.js -c unsafe=true,unused=false -m --comments /^!/
