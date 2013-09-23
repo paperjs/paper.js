123b6b680d45f36cf64bea9ef7ebbe4c74f8bb4f
Jürg Lehni <juerg@scratchdisk.com>
2013-09-22 18:06:47 -0700

    Start implementing TypeScript declaration file. Work in progress.

---
+++ build/typescript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Paper.js - The Swiss Army Knife of Vector Graphics Scripting.
+# http://paperjs.org/
+#
+# Copyright (c) 2011 - 2013, Juerg Lehni & Jonathan Puckey
+# http://lehni.org/ & http://jonathanpuckey.com/
+#
+# Distributed under the MIT license. See LICENSE file for details.
+#
+# All rights reserved.
+
+# Generates TypeScript declaration file
+
+cd jsdoc-toolkit
+java -jar jsrun.jar app/run.js -c=conf/typescript.conf
+cd ..
