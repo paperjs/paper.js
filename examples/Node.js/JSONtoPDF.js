c08fd44101e171f4f7022c37bbcd34c47db0a3fc
Jürg Lehni <juerg@scratchdisk.com>
2013-11-29 12:17:59 +0100

    Add Node JSONtoPDF example.

---
+++ examples/Node.js/JSONtoPDF.js
@@ -0,0 +1,19 @@
+var paper = require('paper'),
+	path = require('path'),
+	fs = require('fs');
+
+var canvas = new paper.Canvas(612, 792, 'pdf');
+paper.setup(canvas);
+with (paper) {
+	fs.readFile('./in.json', { encoding: 'utf8' }, function (err, data) {
+		if (err)
+			throw err; 
+		project.importJSON(data);
+		view.draw();
+		fs.writeFile(path.resolve(__dirname, 'out.pdf'), canvas.toBuffer(), function (err) {
+			if (err)
+				throw err;
+			console.log('Saved!');
+		});
+	});
+}
