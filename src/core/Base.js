e22fa3a6641ef118a6520069095151f49bfebf02
Jürg Lehni <juerg@scratchdisk.com>
2014-01-04 01:47:16 +0100

    Update copyright notice.

--- src/core/Base.js
+++ src/core/Base.js
@@ -2,8 +2,8 @@
  * Paper.js - The Swiss Army Knife of Vector Graphics Scripting.
  * http://paperjs.org/
  *
- * Copyright (c) 2011 - 2013, Juerg Lehni & Jonathan Puckey
- * http://lehni.org/ & http://jonathanpuckey.com/
+ * Copyright (c) 2011 - 2014, Juerg Lehni & Jonathan Puckey
+ * http://scratchdisk.com/ & http://jonathanpuckey.com/
  *
  * Distributed under the MIT license. See LICENSE file for details.
  *
@@ -15,8 +15,7 @@
  * @class
  * @private
  */
-// Extend Base with utility functions used across the library. Also set
-// this.Base on the injection scope, since straps.js ommits that.
+// Extend Base with utility functions used across the library.
 Base.inject(/** @lends Base# */{
 	/**
 	 * Renders base objects to strings in object literal notation.
