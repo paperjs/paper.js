paperjs/paper.js

commit 0ce89fa47c11868e089e9c127fc2449988d7b9e2

Simplify self-intersecting code in PathItem#getIntersections() by passing on tMin, tMax, uMin & uMax to Curve#getIntersections().

src/path/Curve.js
@@ -1134,7 +1134,8 @@ new function() { // Scope for methods that require numerical integration
 			if ((Curve.isLinear(v1) || Curve.isFlatEnough(v1, tolerance))
 				&& (Curve.isLinear(v2) || Curve.isFlatEnough(v2, tolerance))) {
 				// See if the parametric equations of the lines interesct.
-				addLineIntersection(v1, v2, curve1, curve2, locations);
+				addLineIntersection(v1, v2, curve1, curve2, locations,
+						tMin, tMax, uMin, uMax);
 			} else {
 				// Subdivide both curves, and see if they intersect.
 				// If one of the curves is flat already, no further subdivion
@@ -1144,7 +1145,7 @@ new function() { // Scope for methods that require numerical integration
 				for (var i = 0; i < 2; i++)
 					for (var j = 0; j < 2; j++)
 						Curve.getIntersections(v1s[i], v2s[j], curve1, curve2,
-								locations);
+								locations, tMin, tMax, uMin, uMax);
 			}
 		}
 		return locations;
@@ -1273,7 +1274,8 @@ new function() { // Scope for methods that require numerical integration
 	 * line is on the X axis, and solve the implicit equations for the X axis
 	 * and the curve.
 	 */
-	function addCurveLineIntersections(v1, v2, curve1, curve2, locations) {
+	function addCurveLineIntersections(v1, v2, curve1, curve2, locations,
+			tMin, tMax, uMin, uMax) {
 		var flip = Curve.isLinear(v1),
 			vc = flip ? v2 : v1,
 			vl = flip ? v1 : v2,
@@ -1313,14 +1315,16 @@ new function() { // Scope for methods that require numerical integration
 				var tl = Curve.getParameterOf(rvl, x, 0),
 					t1 = flip ? tl : tc,
 					t2 = flip ? tc : tl;
-				locations.push(new CurveLocation(
-						curve1, t1, Curve.evaluate(v1, t1, 0),
-						curve2, t2, Curve.evaluate(v2, t2, 0)));
+				if (t1 >= tMin && t1 <= tMax && t2 >= uMin && t2 <= uMax)
+					locations.push(new CurveLocation(
+							curve1, t1, Curve.evaluate(v1, t1, 0),
+							curve2, t2, Curve.evaluate(v2, t2, 0)));
 			}
 		}
 	}
 
-	function addLineIntersection(v1, v2, curve1, curve2, locations) {
+	function addLineIntersection(v1, v2, curve1, curve2, locations,
+				tMin, tMax, uMin, uMax) {
 		var point = Line.intersect(
 				v1[0], v1[1], v1[6], v1[7],
 				v2[0], v2[1], v2[6], v2[7]);
@@ -1329,7 +1333,10 @@ new function() { // Scope for methods that require numerical integration
 			// since they will be used for sorting
 			var t1 = Curve.getParameterOf(v1, point.x, point.y),
 				t2 = Curve.getParameterOf(v2, point.x, point.y);
-			locations.push(new CurveLocation(curve1, t1, point, curve2, t2, point));
+			if (t1 >= tMin && t1 <= tMax && t2 >= uMin && t2 <= uMax)
+				locations.push(new CurveLocation(
+						curve1, t1, point,
+						curve2, t2, point));
 		}
 	}
 
@@ -1337,7 +1344,8 @@ new function() { // Scope for methods that require numerical integration
 		// We need to provide the original left curve reference to the
 		// #getIntersections() calls as it is required to create the resulting
 		// CurveLocation objects.
-		getIntersections: function(v1, v2, curve1, curve2, locations) {
+		getIntersections: function(v1, v2, curve1, curve2, locations,
+				tMin, tMax, uMin, uMax) {
 			var linear1 = Curve.isLinear(v1),
 				linear2 = Curve.isLinear(v2);
 			(linear1 && linear2
@@ -1347,8 +1355,10 @@ new function() { // Scope for methods that require numerical integration
 					: addCurveIntersections)(v1, v2, curve1, curve2, locations,
 						// Define the defaults for these parameters of
 						// addCurveIntersections():
-						// tMin, tMax, uMin, uMax, oldTDiff, reverse, recursion
-						0, 1, 0, 1, 0, false, 0);
+						// tMin, tMax, uMin, uMax
+						tMin || 0, tMax || 1, uMin || 0, uMax || 1,
+						// oldTDiff, reverse, recursion
+						0, false, 0);
 			return locations;
 		}
 	}};

src/path/PathItem.js
@@ -69,7 +69,6 @@ var PathItem = Item.extend(/** @lends PathItem# */{
 		if (!selfOp && !this.getBounds().touches(path.getBounds()))
 			return [];
 		var locations = [],
-			locs = [],
 			curves1 = this.getCurves(),
 			curves2 = selfOp ? curves1 : path.getCurves(),
 			matrix1 = this._matrix.orNullIfIdentity(),
@@ -96,37 +95,30 @@ var PathItem = Item.extend(/** @lends PathItem# */{
 				// property.
 				if (new Line(seg1._point.subtract(h1), h1, true).intersect(
 						new Line(seg2._point.subtract(h2), h2, true), false)) {
-					locs.length = 0;
-					var parts = Curve.subdivide(values1);
+					var parts = Curve.subdivide(values1),
+						before = locations.length;
 					Curve.getIntersections(parts[0], parts[1], curve1, curve1,
-							locs);
-					for (var j = locs.length - 1; j >= 0; j--) {
-						var loc = locs[j];
-						if (loc._parameter <= ONE) {
-							loc._parameter /= 2;
-							loc._parameter2 = (loc._parameter2 + 1) / 2;
-							locations.push(loc);
-							break;
-						}
+							locations, 0, ONE); // tMax
+					// Check if a location was added by comparing length before.
+					if (locations.length > before) {
+						var loc = locations[before];
+						// Since the curve has split itself, we need to adjust
+						// the parameters for both locations.
+						loc._parameter /= 2;
+						loc._parameter2 = 0.5 + loc._parameter2 / 2;
 					}
 				}
 			}
 			// Check for intersections with other curves
 			for (var j = selfOp ? i + 1 : 0; j < length2; j++) {
-				// Avoid end point intersections on consecutive curves
-				if (selfOp && (j === i + 1 || (j === length2 - 1 && i === 0))) {
-					locs.length = 0;
-					Curve.getIntersections(values1, values2[j], curve1,
-							curves2[j], locs);
-					for (var k = locs.length - 1; k >= 0; k--) {
-						var loc = locs[k],
-							t = loc.getParameter();
-						if (t > ZERO && t < ONE)
-							locations.push(loc);
-					}
-				} else {
-					Curve.getIntersections(values1, values2[j], curve1,
-							curves2[j], locations);
+				// Avoid end point intersections on consecutive curves whe self
+				// intersecting.
+				var excludeEnds = selfOp && (j === i + 1
+						|| (j === length2 - 1 && i === 0));
+				Curve.getIntersections(values1, values2[j], curve1,
+						curves2[j], locations,
+						excludeEnds ? ZERO : 0, // tMin
+						excludeEnds ? ONE : 1); // tMax
 				}
 			}
 		}

commit f4baf690f827428b81c173cdc7941fba9c6c99ec

Fix issue introduced in 0ce89fa47c11868e089e9c127fc2449988d7b9e2

src/path/Curve.js
@@ -1037,7 +1037,14 @@ new function() { // Scope for methods that require numerical integration
 		}
 	};
 }, new function() { // Scope for intersection using bezier fat-line clipping
-	function addCurveIntersections(v1, v2, curve1, curve2, locations,
+	function addLocation(locations, include, curve1, t1, point1, curve2, t2,
+			point2) {
+		var loc = new CurveLocation(curve1, t1, point1, curve2, t2, point2);
+		if (!include || include(loc))
+			locations.push(loc);
+	}
+
+	function addCurveIntersections(v1, v2, curve1, curve2, locations, include,
 			tMin, tMax, uMin, uMax, oldTDiff, reverse, recursion) {
 /*#*/ if (__options.fatline) {
 		// Avoid deeper recursion.
@@ -1098,16 +1105,20 @@ new function() { // Scope for methods that require numerical integration
 			if (tMaxNew - tMinNew > uMax - uMin) {
 				var parts = Curve.subdivide(v1, 0.5),
 					t = tMinNew + (tMaxNew - tMinNew) / 2;
-				addCurveIntersections(v2, parts[0], curve2, curve1, locations,
+				addCurveIntersections(
+					v2, parts[0], curve2, curve1, locations, include,
 					uMin, uMax, tMinNew, t, tDiff, !reverse, ++recursion);
-				addCurveIntersections(v2, parts[1], curve2, curve1, locations,
+				addCurveIntersections(
+					v2, parts[1], curve2, curve1, locations, include,
 					uMin, uMax, t, tMaxNew, tDiff, !reverse, recursion);
 			} else {
 				var parts = Curve.subdivide(v2, 0.5),
 					t = uMin + (uMax - uMin) / 2;
-				addCurveIntersections(parts[0], v1, curve2, curve1, locations,
+				addCurveIntersections(
+					parts[0], v1, curve2, curve1, locations, include,
 					uMin, t, tMinNew, tMaxNew, tDiff, !reverse, ++recursion);
-				addCurveIntersections(parts[1], v1, curve2, curve1, locations,
+				addCurveIntersections(
+					parts[1], v1, curve2, curve1, locations, include,
 					t, uMax, tMinNew, tMaxNew, tDiff, !reverse, recursion);
 			}
 		} else if (Math.max(uMax - uMin, tMaxNew - tMinNew) < tolerance) {
@@ -1115,16 +1126,16 @@ new function() { // Scope for methods that require numerical integration
 			var t1 = tMinNew + (tMaxNew - tMinNew) / 2,
 				t2 = uMin + (uMax - uMin) / 2;
 			if (reverse) {
-				locations.push(new CurveLocation(
+				addLocation(locations, include,
 						curve2, t2, Curve.evaluate(v2, t2, 0),
-						curve1, t1, Curve.evaluate(v1, t1, 0)));
+						curve1, t1, Curve.evaluate(v1, t1, 0));
 			} else {
-				locations.push(new CurveLocation(
+				addLocation(locations, include,
 						curve1, t1, Curve.evaluate(v1, t1, 0),
-						curve2, t2, Curve.evaluate(v2, t2, 0)));
+						curve2, t2, Curve.evaluate(v2, t2, 0));
 			}
 		} else { // Iterate
-			addCurveIntersections(v2, v1, curve2, curve1, locations,
+			addCurveIntersections(v2, v1, curve2, curve1, locations, include,
 					uMin, uMax, tMinNew, tMaxNew, tDiff, !reverse, ++recursion);
 		}
 /*#*/ } else { // !__options.fatline
@@ -1140,8 +1151,7 @@ new function() { // Scope for methods that require numerical integration
 			if ((Curve.isLinear(v1) || Curve.isFlatEnough(v1, tolerance))
 				&& (Curve.isLinear(v2) || Curve.isFlatEnough(v2, tolerance))) {
 				// See if the parametric equations of the lines interesct.
-				addLineIntersection(v1, v2, curve1, curve2, locations,
-						tMin, tMax, uMin, uMax);
+				addLineIntersection(v1, v2, curve1, curve2, locations, include);
 			} else {
 				// Subdivide both curves, and see if they intersect.
 				// If one of the curves is flat already, no further subdivion
@@ -1150,11 +1160,10 @@ new function() { // Scope for methods that require numerical integration
 					v2s = Curve.subdivide(v2);
 				for (var i = 0; i < 2; i++)
 					for (var j = 0; j < 2; j++)
-						Curve.getIntersections(v1s[i], v2s[j], curve1, curve2,
-								locations, tMin, tMax, uMin, uMax);
+						addCurveIntersections(v1s[i], v2s[j], curve1, curve2,
+								locations, include);
 			}
 		}
-		return locations;
 /*#*/ } // !__options.fatline
 	}
 
@@ -1281,7 +1290,7 @@ new function() { // Scope for methods that require numerical integration
 	 * and the curve.
 	 */
 	function addCurveLineIntersections(v1, v2, curve1, curve2, locations,
-			tMin, tMax, uMin, uMax) {
+			include) {
 		var flip = Curve.isLinear(v1),
 			vc = flip ? v2 : v1,
 			vl = flip ? v1 : v2,
@@ -1321,28 +1330,25 @@ new function() { // Scope for methods that require numerical integration
 				var tl = Curve.getParameterOf(rvl, x, 0),
 					t1 = flip ? tl : tc,
 					t2 = flip ? tc : tl;
-				if (t1 >= tMin && t1 <= tMax && t2 >= uMin && t2 <= uMax)
-					locations.push(new CurveLocation(
-							curve1, t1, Curve.evaluate(v1, t1, 0),
-							curve2, t2, Curve.evaluate(v2, t2, 0)));
+				addLocation(locations, include,
+						curve1, t1, Curve.evaluate(v1, t1, 0),
+						curve2, t2, Curve.evaluate(v2, t2, 0));
 			}
 		}
 	}
 
-	function addLineIntersection(v1, v2, curve1, curve2, locations,
-				tMin, tMax, uMin, uMax) {
+	function addLineIntersection(v1, v2, curve1, curve2, locations, include) {
 		var point = Line.intersect(
 				v1[0], v1[1], v1[6], v1[7],
 				v2[0], v2[1], v2[6], v2[7]);
-		if (point){
+		if (point) {
 			// We need to return the parameters for the intersection,
 			// since they will be used for sorting
 			var t1 = Curve.getParameterOf(v1, point.x, point.y),
 				t2 = Curve.getParameterOf(v2, point.x, point.y);
-			if (t1 >= tMin && t1 <= tMax && t2 >= uMin && t2 <= uMax)
-				locations.push(new CurveLocation(
-						curve1, t1, point,
-						curve2, t2, point));
+			addLocation(locations, include,
+					curve1, t1, point,
+					curve2, t2, point);
 		}
 	}
 
@@ -1350,21 +1356,19 @@ new function() { // Scope for methods that require numerical integration
 		// We need to provide the original left curve reference to the
 		// #getIntersections() calls as it is required to create the resulting
 		// CurveLocation objects.
-		getIntersections: function(v1, v2, curve1, curve2, locations,
-				tMin, tMax, uMin, uMax) {
+		getIntersections: function(v1, v2, curve1, curve2, locations, include) {
 			var linear1 = Curve.isLinear(v1),
 				linear2 = Curve.isLinear(v2);
 			(linear1 && linear2
 				? addLineIntersection
 				: linear1 || linear2
 					? addCurveLineIntersections
-					: addCurveIntersections)(v1, v2, curve1, curve2, locations,
+					: addCurveIntersections)(
+						v1, v2, curve1, curve2, locations, include,
 						// Define the defaults for these parameters of
 						// addCurveIntersections():
-						// tMin, tMax, uMin, uMax
-						tMin || 0, tMax || 1, uMin || 0, uMax || 1,
-						// oldTDiff, reverse, recursion
-						0, false, 0);
+						// tMin, tMax, uMin, uMax, oldTDiff, reverse, recursion
+						0, 1, 0, 1, 0, false, 0);
 			return locations;
 		}
 	}};

src/path/PathItem.js
@@ -102,32 +102,34 @@ var PathItem = Item.extend(/** @lends PathItem# */{
 						h2.multiply(2), true), false)) {
 					// Self intersectin is found by dividng the curve in two and
 					// and then applying the normal curve intersection code.
-					var parts = Curve.subdivide(values1),
-						before = locations.length;
-					Curve.getIntersections(parts[0], parts[1], curve1, curve1,
-							locations, 0, MAX); // tMax
-					// Check if a location was added by comparing length before.
-					// Self-intersection can only lead to 0 or 1 intersections.
-					if (locations.length > before) {
-						var loc = locations[before];
-						// Since the curve was split above, we need to adjust
-						// the parameters for both locations.
-						loc._parameter /= 2;
-						loc._parameter2 = 0.5 + loc._parameter2 / 2;
-					}
+					var parts = Curve.subdivide(values1);
+					Curve.getIntersections(
+						parts[0], parts[1], curve1, curve1, locations,
+						function(loc) {
+							if (loc._parameter <= MAX) {
+								// Since the curve was split above, we need to
+								// adjust the parameters for both locations.
+								loc._parameter /= 2;
+								loc._parameter2 = 0.5 + loc._parameter2 / 2;
+								return true;
+							}
+						}
+					);
 				}
 			}
 			// Check for intersections with other curves. For self intersection,
 			// we can start at i + 1 instead of 0
 			for (var j = path ? 0 : i + 1; j < length2; j++) {
-				// Avoid end point intersections on consecutive curves whe self
-				// intersecting.
-				var excludeEnds = !path
-						&& (j === i + 1 || j === length2 - 1 && i === 0);
-				Curve.getIntersections(values1, values2[j], curve1,
-						curves2[j], locations,
-						excludeEnds ? MIN : 0, // tMin
-						excludeEnds ? MAX : 1); // tMax
+				Curve.getIntersections(
+					values1, values2[j], curve1, curves2[j], locations,
+					// Avoid end point intersections on consecutive curves whe
+					// self intersecting.
+					!path && (j === i + 1 || j === length2 - 1 && i === 0)
+						&& function(loc) {
+							var t = loc._parameter;
+							return t >= MIN && t <= MAX;
+						}
+				);
 			}
 		}
 		// Now filter the locations and process _expand: