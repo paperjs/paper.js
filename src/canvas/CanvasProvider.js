3dae48acf43f8ce1d41ccb6163599ed72b3e9582
Jürg Lehni <juerg@scratchdisk.com>
2014-03-18 12:42:28 +0100

    Bring back accidentally removed support for pixelRatio in CanvasProvider.

--- src/canvas/CanvasProvider.js
+++ src/canvas/CanvasProvider.js
@@ -15,12 +15,19 @@
 var CanvasProvider = {
 	canvases: [],
 
-	getCanvas: function(width, height) {
+	getCanvas: function(width, height, pixelRatio) {
 		var canvas,
 			init = true;
 		if (typeof width === 'object') {
+			pixelRatio = height;
 			height = width.height;
 			width = width.width;
+		}
+		if (!pixelRatio) {
+			pixelRatio = 1;
+		} else if (pixelRatio !== 1) {
+			width *= pixelRatio;
+			height *= pixelRatio;
 		}
 		if (this.canvases.length) {
 			canvas = this.canvases.pop();
@@ -45,11 +52,13 @@ var CanvasProvider = {
 		}
 		// We save on retrieval and restore on release.
 		ctx.save();
+		if (pixelRatio !== 1)
+			ctx.scale(pixelRatio, pixelRatio);
 		return canvas;
 	},
 
-	getContext: function(width, height) {
-		return this.getCanvas(width, height).getContext('2d');
+	getContext: function(width, height, pixelRatio) {
+		return this.getCanvas(width, height, pixelRatio).getContext('2d');
 	},
 
 	 // release can receive either a canvas or a context.
