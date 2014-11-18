912f7ad62953a807c642aa8f7ec3247f3b4fb579
Jürg Lehni <juerg@scratchdisk.com>
2014-11-18 15:24:40 -0800

    Allow injection of additional undefined fields through object literal constructors and #set().

--- src/core/Base.js
+++ src/core/Base.js
@@ -68,8 +68,7 @@ Base.inject(/** @lends Base# */{
             // inheritance trick in the argument reading code.
             var orig = props._filtering || props;
             for (var key in orig) {
-                if (key in this && orig.hasOwnProperty(key)
-                        && !(exclude && exclude[key])) {
+                if (orig.hasOwnProperty(key) && !(exclude && exclude[key])) {
                     var value = props[key];
                     // Due to the _filtered inheritance trick, undefined is used
                     // to mask already consumed named arguments.
