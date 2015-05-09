ac8e92a0c9a5cd6eca6d5b71179d3a7dc126c136
Skalkaz <randomwag@gmail.com>
2015-05-09 16:59:29 +0200

    Update DomElement.js

--- src/dom/DomElement.js
+++ src/dom/DomElement.js
@@ -58,6 +58,7 @@ var DomElement = new function() {
                 rect = { left: 0, top: 0, width: 0, height: 0 };
                 var x = rect.left - 0,
                 y = rect.top - 0;    
+                throw "clientLeft error caught"
             }
             if (!viewport) {
                 var view = doc.defaultView;
