098ddda3bce25c0b8d01b22e6d972c0a0deb8873
Jürg Lehni <juerg@scratchdisk.com>
2015-08-18 22:51:34 +0200

    Include copyright comment in gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,15 @@
+/*
+ * Paper.js - The Swiss Army Knife of Vector Graphics Scripting.
+ * http://paperjs.org/
+ *
+ * Copyright (c) 2011 - 2014, Juerg Lehni & Jonathan Puckey
+ * http://scratchdisk.com/ & http://jonathanpuckey.com/
+ *
+ * Distributed under the MIT license. See LICENSE file for details.
+ *
+ * All rights reserved.
+ */
+
 var gulp = require('gulp'),
     qunit = require('gulp-qunit');
 
