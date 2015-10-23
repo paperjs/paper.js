502558a3212af5ef22efa34e6b26eabd4f857cca
sapics <gv.nishino@gmail.com>
2015-10-23 09:42:58 +0900

    Fix variable leaks

--- src/style/Color.js
+++ src/style/Color.js
@@ -578,7 +578,7 @@ var Color = Base.extend(new function() {
                                     ? 'gray'
                                     : 'rgb';
                         // Convert to array and parse in one loop, for efficiency
-                        var properties = types[type];
+                        var properties = types[type],
                             parsers = componentParsers[type];
                         this._components = components = [];
                         for (var i = 0, l = properties.length; i < l; i++) {
