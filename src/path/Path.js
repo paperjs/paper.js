09d4de6a1b0bcb2ba279be2f83112d58c9e2b9dc
Tomas Junnonen <tomas1@gmail.com>
2015-10-28 12:03:12 -0400

    Issue 815: Cannot add segment to Path after removing last segment
    Fixed an issue where after removing the last segment of a path,
    new segments cannot be re-added.
    
    Added new Path Curve test cases to test the difference between
    a path with one and no segments (zero curves in both cases), which
    catches the bug.

--- src/path/Path.js
+++ src/path/Path.js
@@ -417,7 +417,7 @@ var Path = PathItem.extend(/** @lends Path# */{
             var total = this._countCurves(),
                 // If we're adding a new segment to the end of an open path,
                 // we need to step one index down to get its curve.
-                from = index + amount - 1 === total ? index - 1 : index,
+                from = Math.max(index + amount - 1 === total ? index - 1 : index, 0),
                 start = from,
                 to = Math.min(from + amount, total);
             if (segs._curves) {
