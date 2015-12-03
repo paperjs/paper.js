9c1c00c11eb3d114b6b9dbc3e698cef2ec9c7d5c
sapics <gv.nishino@gmail.com>
2015-12-03 14:42:27 +0900

    Minor minification

--- src/path/CompoundPath.js
+++ src/path/CompoundPath.js
@@ -136,8 +136,9 @@ var CompoundPath = PathItem.extend(/** @lends CompoundPath# */{
     },
 
     smooth: function() {
-        for (var i = 0, l = this._children.length; i < l; i++)
-            this._children[i].smooth();
+        var children = this._children;
+        for (var i = 0, l = children.length; i < l; i++)
+            children[i].smooth();
     },
 
     // DOCS: reduce()
