daaf625ebd8b7399bf6fb6628017c78efa4e707f
Jürg Lehni <juerg@scratchdisk.com>
2016-01-04 10:02:58 +0100

    Double GEOMETRIC_EPSILON to cover the last found edge case.
    See https://github.com/paperjs/paper.js/issues/784#issuecomment-168614053

--- src/util/Numerical.js
+++ src/util/Numerical.js
@@ -103,7 +103,7 @@ var Numerical = new function() {
          * The epsilon to be used when performing "geometric" checks, such as
          * distances between points and lines.
          */
-        GEOMETRIC_EPSILON: 2e-7,
+        GEOMETRIC_EPSILON: 4e-7,
         /**
          * The epsilon to be used when performing winding contribution checks.
          */
