fb7606524278f1604d33cc11e10e4ca5decaf682
Evgeniy Malyarov <info@oknosoft.ru>
2016-01-11 15:52:40 +0500

    Update Curve.js
    fix getNearestPoint error on uninserted curves

--- src/path/Curve.js
+++ src/path/Curve.js
@@ -1121,7 +1121,8 @@ statics: /** @lends Curve */{
      * specified point
      */
     getNearestPoint: function(/* point */) {
-        return this.getNearestLocation.apply(this, arguments).getPoint();
+        var loc = this.getNearestLocation.apply(this, arguments);
+		return loc ? loc.getPoint() : loc;
     }
 
     /**
