1dcb19ec3bbdc87bc765ee79783ceb43c9eef8c6
Jürg Lehni <juerg@scratchdisk.com>
2016-01-25 11:35:15 +0100

    Implement jshint and watch task.

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,27 @@
+/*
+ * Paper.js - The Swiss Army Knife of Vector Graphics Scripting.
+ * http://paperjs.org/
+ *
+ * Copyright (c) 2011 - 2016, Juerg Lehni & Jonathan Puckey
+ * http://scratchdisk.com/ & http://jonathanpuckey.com/
+ *
+ * Distributed under the MIT license. See LICENSE file for details.
+ *
+ * All rights reserved.
+ */
+
+var gulp = require('gulp'),
+    path = require('path'),
+    gutil = require('gulp-util');
+
+
+gulp.task('watch', function () {
+    gulp.watch('src/**/*.js', ['jshint'])
+        .on('change', function(event) {
+            gutil.log(
+                gutil.colors.green('File ' + event.type + ': ') +
+                gutil.colors.magenta(path.basename(event.path))
+            );
+        });
+});
+
