778248352fd7c201a8250fafe6dfb0a5ee578b81
Jürg Lehni <juerg@scratchdisk.com>
2016-01-28 00:22:06 +0100

    Write more comments explaining the handling of node-qunit.

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -37,23 +37,29 @@ gulp.task('test:node', ['minify:acorn'], function(callback) {
         log: extend({ errors: true }, options)
     });
     // Use the correct working directory for tests:
-    process.chdir('test');
+    process.chdir('./test');
     qunit_node.run({
         maxBlockDuration: 100 * 1000,
         deps: [
-            // To dynamically load the tests from the sources, we need to
-            // require Prepro.js first. Since we need a sub-module, we need to
+            // To dynamically load the tests files from the sources, we need to
+            // require Prepro.js first. Since we need a sub-module, we have to
             // use relative addresses: require('prepro/lib/node') does not work
             // because of the way node-qunit handles relative addresses.
             '../node_modules/prepro/lib/node.js',
+            // Note that loading dist/paper-full.js also works in combination
+            // with `gulp load`, in which case Prepro.js is present and handles
+            // the loading transparently.
             { path: '../dist/paper-full.js', namespace: 'paper' }
         ],
+        // Now load the actual test files through test/load.js, using Prepro.js
+        // for the loading, which was requested above.
         code: 'load.js'
     }, function(err, stats) {
         var result;
         if (err) {
             result = new gutil.PluginError(name, err);
         } else {
+            // Imitate the way gulp-qunit formats results and errors.
             var color = gutil.colors[stats.failed > 0 ? 'red' : 'green'];
             gutil.log('Took ' + stats.runtime + ' ms to run ' +
                 gutil.colors.blue(stats.assertions) + ' tests. ' +
