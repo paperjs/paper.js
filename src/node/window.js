e1cfb39b2e1767252b64eb228474b595f264e2a3
Jürg Lehni <juerg@scratchdisk.com>
2016-01-28 05:13:04 +0100

    Imrpove DOMParser#parseFromString() polyfill for Node.js
    And no need to move imported SVG nodes into document, since we don't have styling in Node.js

--- src/node/window.js
+++ src/node/window.js
@@ -51,9 +51,18 @@ function DOMParser() {
 }
 
 DOMParser.prototype.parseFromString = function(string, contenType) {
-    var div = document.createElement('div');
-    div.innerHTML = string;
-    return div.firstChild;
+    // Create a new document, since we're supposed to always return one.
+    var doc = document.implementation.createHTMLDocument(''),
+        body = doc.body,
+        last;
+    // Set the body's HTML, then change the DOM according the specs.
+    body.innerHTML = string;
+    // Remove all top-level children (<html><head/><body/></html>)
+    while (last = doc.lastChild)
+        doc.removeChild(last);
+    // Insert the first child of the body at the top.
+    doc.appendChild(body.firstChild);
+    return doc;
 };
 
 window.XMLSerializer = XMLSerializer;
