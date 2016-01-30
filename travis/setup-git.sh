89730128aa1ce5bd5ddeddf372d93ce57383fbc8
Jürg Lehni <juerg@scratchdisk.com>
2016-01-30 17:03:25 +0100

    Finish work on automatic deployment of prebuilt versions on each commit to prebuilt/dist an prebuilt/module branches.
    Closes #629

--- travis/setup-git.sh
+++ travis/setup-git.sh
@@ -12,9 +12,9 @@ echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 # It took ages to figures this one out:
 # Travis CI sets up the origin to only fetch the specific branch, e.g.:
 #     fetch = +refs/heads/develop:refs/remotes/origin/develop
-# Since we want to deploy to /prebuilt/module also, we need to change that:
+# Since we want to deploy to prebuilt/module also, we need to change that:
 #     fetch = +refs/heads/*:refs/remotes/origin/*
-# We can change that by removing and adding the origin again:
+# We can change the fetch setting by removing and adding the origin again:
 git remote remove origin
 git remote add origin https://github.com/paperjs/paper.js.git
 
