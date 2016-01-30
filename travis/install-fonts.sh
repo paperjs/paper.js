e14a50c820bd3d979ced7f2cb1c713d900513363
Jürg Lehni <juerg@scratchdisk.com>
2016-01-30 04:01:26 +0100

    Travis CI: Include fons in the cache.
    And add more comments to script files.

--- travis/install-fonts.sh
+++ travis/install-fonts.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-wget http://sourceforge.net/p/tellmatic/git/ci/master/tree/img/arial.ttf?format=raw -O arial.ttf
-mkdir -p ~/.fonts
-mv arial.ttf ~/.fonts
+
+# Download and instal fonts, but only if they haven't been installed from the
+# cache yet.
+
+if [ ! -d ~/.fonts ];
+    wget http://sourceforge.net/p/tellmatic/git/ci/master/tree/img/arial.ttf?format=raw -O arial.ttf
+    mkdir -p ~/.fonts
+    mv arial.ttf ~/.fonts
+fi;
 fc-cache -f -v
--- travis/setup-git.sh
+++ travis/setup-git.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Setup the git user with the right credentials so we can push to the paper.js
+# repository. The GH_TOKEN is generated as a secure environment variable:
+#
+# travis encrypt GH_TOKEN=<TOKEN> --add
+
 git config user.name "Paper.js Bot"
 git config user.email "bot@paperjs.org"
 git config credential.helper "store --file=.git/credentials"
