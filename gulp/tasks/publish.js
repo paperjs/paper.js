1b1b9a16066403228f88c1da983fd6af3f5c8021
Jürg Lehni <juerg@scratchdisk.com>
2016-07-09 19:48:02 +0200

    Gulp: Change publish task so that dist folder contains built versions on master branch.
    As required by Bower...

--- gulp/tasks/publish.js
+++ gulp/tasks/publish.js
@@ -11,31 +11,53 @@
  */
 
 var gulp = require('gulp'),
-    addSrc = require('gulp-add-src'),
     bump = require('gulp-bump'),
     git = require('gulp-git-streamed'),
+    run = require('run-sequence'),
     shell = require('gulp-shell'),
     options = require('../utils/options.js')({ suffix: false });
+
+gulp.task('publish', function() {
+    if (options.branch !== 'develop') {
+        throw new Error('Publishing is only allowed on the develop branch.');
+    }
+    return run(
+        'publish:bump',
+        'publish:dist',
+        'publish:commit',
+        'publish:release',
+        'publish:load'
+    );
+});
 
 gulp.task('publish:bump', function() {
     return gulp.src([ 'package.json', 'component.json' ])
         .pipe(bump({ version: options.version }))
-        .pipe(gulp.dest('./'))
-        .pipe(addSrc('src/options.js'))
-        .pipe(git.add());
+        .pipe(gulp.dest('.'));
 });
 
-gulp.task('publish', ['publish:bump'], function() {
-    if (options.branch !== 'develop') {
-        throw new Error('Publishing is only allowed on the develop branch.');
-    }
+gulp.task('publish:dist', ['dist']);
+
+gulp.task('publish:commit', function() {
     var message = 'Release version ' + options.version;
     return gulp.src('.')
+        .pipe(git.add())
         .pipe(git.commit(message))
-        .pipe(git.tag('v' + options.version, message))
+        .pipe(git.tag('v' + options.version, message));
+});
+
+gulp.task('publish:release', function() {
+    return gulp.src('.')
         .pipe(git.checkout('master'))
         .pipe(git.merge('develop', { args: '-X theirs' }))
         .pipe(git.push('origin', ['master', 'develop'], { args: '--tags' }))
         .pipe(shell('npm publish'))
         .pipe(git.checkout('develop'));
+});
+
+gulp.task('publish:load', ['load'], function() {
+    return gulp.src('dist')
+        .pipe(git.add())
+        .pipe(git.commit('Switch back to load.js versions for development.'))
+        .pipe(git.push('origin', 'develop'));
 });
