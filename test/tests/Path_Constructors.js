2290ec0ff6713752b6a02da09c7556bc1a28a782
Jürg Lehni <juerg@scratchdisk.com>
2017-04-10 09:30:52 +0200

    Improve unit test for new Path.Constructor({ insert: false })

--- test/tests/Path_Constructors.js
+++ test/tests/Path_Constructors.js
@@ -131,7 +131,19 @@ test('new Path.Line({ from: from, to: to })', function() {
     equals(path.segments.toString(), '{ point: { x: 20, y: 20 } },{ point: { x: 40, y: 40 } }');
 });
 
-test('new Path.Line({insert: false, from:[50, 50], to:[100, 100]})', function() {
-    var path = new Path.Line({insert: false, from:[50, 50], to:[100, 100]});
-    equals(path.insert === false, false);
+test('new Path.Line({ insert: false })', function() {
+    var path = new Path.Line({
+        from:[50, 50],
+        to:[100, 100],
+        insert: false
+    });
+    equals(function() {
+        return typeof path.insert;
+    }, 'function');
+    equals(function() {
+        return path.parent;
+    }, null);
+    equals(function() {
+        return path.isInserted();
+    }, false);
 });
