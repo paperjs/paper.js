8d0fecbb1b251cba4a68c8f2c2f775e1061e4a06
DD <liudi08@gmail.com>
2018-06-27 16:29:46 -0400

    For SVG import, If second argument is missing in translate, assume that it's zero. Currently it assumes that y matches x if y is missing.

--- src/svg/SvgImport.js
+++ src/svg/SvgImport.js
@@ -339,7 +339,8 @@ new function() {
                     matrix.rotate(v[0], v[1], v[2]);
                     break;
                 case 'translate':
-                    matrix.translate(v[0], v[1]);
+                    var v1 = (typeof v[1] === 'number') ? v[1] : 0;
+                    matrix.translate(v[0], v1);
                     break;
                 case 'scale':
                     matrix.scale(v);
