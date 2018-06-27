bf63bf223c9aaa4b2e5b406d05a83d1262fee4d0
DD <liudi08@gmail.com>
2018-06-27 16:47:19 -0400

    Add rotate case as well

--- src/svg/SvgImport.js
+++ src/svg/SvgImport.js
@@ -336,7 +336,8 @@ new function() {
                             new Matrix(v[0], v[1], v[2], v[3], v[4], v[5]));
                     break;
                 case 'rotate':
-                    matrix.rotate(v[0], v[1], v[2]);
+                    var v2 = (typeof v[1] === 'number' && typeof v[2] !== 'number') ? 0 : v[2];
+                    matrix.rotate(v[0], v[1], v2);
                     break;
                 case 'translate':
                     var v1 = (typeof v[1] === 'number') ? v[1] : 0;
