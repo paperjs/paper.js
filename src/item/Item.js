paperjs/paper.js

commit 34679614c0db77387c72fb50e35daac6882e941e

Fix: selected position is wrongly placed when item is in a group with matrix not applied

src/item/Item.js
@@ -4411,7 +4411,7 @@ new function() { // Injection scope for hit-test functions shared with project
             if (itemSelected)
                 this._drawSelected(ctx, mx, selectionItems);
             if (positionSelected) {
-                var point = this.getPosition(true),
+                var point = mx._transformPoint(this.getPosition(true)),
                     x = point.x,
                     y = point.y;
                 ctx.beginPath();

commit 9020cbbc2309685a7acee734794fc647ab5d8eb7

Fix item selected position with matrix not applied
Make sure selected position is drawn in global coordinates system
whether item is in a group or not and whether matrix is applied or not.
Correct a bug introduced in 34679614c0db77387c72fb50e35daac6882e941e.

src/item/Item.js
@@ -4423,7 +4423,9 @@ new function() { // Injection scope for hit-test functions shared with project
             if (itemSelected)
                 this._drawSelected(ctx, mx, selectionItems);
             if (positionSelected) {
-                var point = mx._transformPoint(this.getPosition(true)),
+                // Get position in global coordinates system
+                var pos = this.getPosition(true),
+                    point = this.parent ? this.parent.localToGlobal(pos) : pos,
                     x = point.x,
                     y = point.y;
                 ctx.beginPath();