3e15cf09c2f708887400d379434d04036b283233
sapics <gv.nishino@gmail.com>
2018-10-15 08:59:53 +0900

    Trim space in jsdoc comment

--- src/item/Item.js
+++ src/item/Item.js
@@ -502,7 +502,7 @@ new function() { // Injection scope for various item event handlers
      *     light', 'color-dodge', 'color-burn', 'darken', 'lighten',
      *     'difference', 'exclusion', 'hue', 'saturation', 'luminosity',
      *     'color', 'add', 'subtract', 'average', 'pin-light', 'negation',
-     *     'source- over', 'source-in', 'source-out', 'source-atop',
+     *     'source-over', 'source-in', 'source-out', 'source-atop',
      *     'destination-over', 'destination-in', 'destination-out',
      *     'destination-atop', 'lighter', 'darker', 'copy', 'xor'
      * @default 'normal'
