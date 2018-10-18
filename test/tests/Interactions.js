paperjs/paper.js

commit fbd5eeb7ef12fe5f30565dd2b9fcdac3631c1e54

Mouse Events: Fix item-based doubleclick events.
Closes #1316

src/view/View.js
@@ -1271,12 +1271,11 @@ new function() { // Injection scope for event handling on the browser
                     point, prevPoint)
             // Next handle the hit-item, if it's different from the drag-item
             // and not a descendant of it (in which case it would already have
-            // received an event in the call above). Use fallbacks to translate
-            // mousedrag to mousemove, since drag is handled above.
+            // received an event in the call above).
             || hitItem && hitItem !== dragItem
                 && !hitItem.isDescendant(dragItem)
-                && emitMouseEvent(hitItem, null, fallbacks[type] || type, event,
-                    point, prevPoint, dragItem)
+                && emitMouseEvent(hitItem, null, type, event, point, prevPoint,
+                    dragItem)
             // Lastly handle the mouse events on the view, if we're still here.
             || emitMouseEvent(view, dragItem || hitItem || view, type, event,
                     point, prevPoint));

commit 9f89be424b4fdf5e9eb75dda6bfe49057cfc18c6

Fix mousedrag error with in front items
When dragging item A behind item B, mousedrag event was emitted on
item B instead of mousemove.
Bug introduced in fbd5eeb
Closes #1465

src/view/View.js
@@ -1271,11 +1271,12 @@ new function() { // Injection scope for event handling on the browser
                     point, prevPoint)
             // Next handle the hit-item, if it's different from the drag-item
             // and not a descendant of it (in which case it would already have
-            // received an event in the call above).
+            // received an event in the call above). Translate mousedrag to
+            // mousemove, since drag is handled above.
             || hitItem && hitItem !== dragItem
                 && !hitItem.isDescendant(dragItem)
-                && emitMouseEvent(hitItem, null, type, event, point, prevPoint,
-                    dragItem)
+                && emitMouseEvent(hitItem, null, type === 'mousedrag' ?
+                'mousemove' : type, event, point, prevPoint, dragItem)
             // Lastly handle the mouse events on the view, if we're still here.
             || emitMouseEvent(view, dragItem || hitItem || view, type, event,
                     point, prevPoint));

test/tests/Interactions.js
@@ -334,3 +334,14 @@ test('Item#onMouseDrag() is not triggered after mouse up', function(assert) {
     expect(1);
 });
 
+test('Item#onMouseDrag() is not triggered if mouse down was on another item', function(assert) {
+    var item = new Path.Rectangle(new Point(0, 0), new Size(10));
+    item.fillColor = 'red';
+    var item2 = item.clone().translate(10);
+    item2.onMouseDrag = function(event) {
+        throw 'this should not be called';
+    };
+    triggerMouseEvent('mousedown', new Point(5, 5));
+    triggerMouseEvent('mousemove', new Point(11, 11));
+    expect(0);
+});