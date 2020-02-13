c044b698c6b224c10a7747664b2a4cd00a416a25
Jürg Lehni <juerg@scratchdisk.com>
2020-02-13 17:06:46 +0100

    Fix comment

--- src/item/Raster.js
+++ src/item/Raster.js
@@ -113,7 +113,7 @@ var Raster = Item.extend(/** @lends Raster# */{
         // - A size (Size) describing the canvas that will be  created and an
         //   optional position (Point).
         // If _initialize can set properties through object literal, we're done.
-        // Otherwise we need to check the type of object:       var image,
+        // Otherwise we need to check the type of object:
         if (!this._initialize(source,
                 position !== undefined && Point.read(arguments))) {
             var image,
