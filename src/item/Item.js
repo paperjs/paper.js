aa787f2b89fbae992bcdf635e8866d0e71da16ea
Jürg Lehni <juerg@scratchdisk.com>
2020-06-22 17:22:41 +0200

    Change Item#raster() to receive options object

--- src/item/Item.js
+++ src/item/Item.js
@@ -1739,15 +1739,26 @@ new function() { // Injection scope for various item event handlers
     },
 
     /**
+     * @name Item#rasterize
+     * @function
+     * @param {Number} [resolution=view.resolution]
+     * @param {Boolean} [insert=true]
+     * @deprecated use {@link #rasterize(options)} instead.
+     */
+    /**
      * Rasterizes the item into a newly created Raster object. The item itself
      * is not removed after rasterization.
      *
-     * @param {Number} [resolution=view.resolution] the resolution of the raster
-     *     in pixels per inch (DPI). If not specified, the value of
-     *     `view.resolution` is used.
-     * @param {Boolean} [insert=true] specifies whether the raster should be
+     * @option [resolution=view.resolution] {Number} the desired resolution to
+     *     be used when rasterizing, in pixels per inch (DPI). If not specified,
+     *     the value of `view.resolution` is used by default.
+     * @option [insert=true] {Boolean} specifies whether the raster should be
      *     inserted into the scene graph. When set to `true`, it is inserted
-     *     above the original
+     *     above the rasterized item.
+     *
+     * @name Item#rasterize
+     * @function
+     * @param {Object} [options={}] the rasterization options
      * @return {Raster} the newly created raster item
      *
      * @example {@paperscript}
@@ -1768,7 +1779,16 @@ new function() { // Injection scope for various item event handlers
      * circle.scale(5);
      * raster.scale(5);
      */
-    rasterize: function(resolution, insert) {
+    rasterize: function(arg0, arg1) {
+        var resolution,
+            insert;
+        if (Base.isPlainObject(arg0)) {
+            resolution = arg0.resolution;
+            insert = arg0.insert;
+        } else {
+            resolution = arg0;
+            insert = arg1;
+        }
         // TODO: Switch to options object for more descriptive call signature.
         var bounds = this.getStrokeBounds(),
             scale = (resolution || this.getView().getResolution()) / 72,
@@ -1779,17 +1799,14 @@ new function() { // Injection scope for various item event handlers
             size = new Size(bottomRight.subtract(topLeft)),
             raster = new Raster(Item.NO_INSERT);
         if (!size.isZero()) {
-            var canvas = CanvasProvider.getCanvas(size.multiply(scale)),
-                ctx = canvas.getContext('2d'),
+            raster.setSize(size.multiply(scale));
+            var ctx = raster.getContext(true),
                 matrix = new Matrix().scale(scale).translate(topLeft.negate());
             ctx.save();
             matrix.applyToContext(ctx);
             // See Project#draw() for an explanation of new Base()
             this.draw(ctx, new Base({ matrices: [matrix] }));
             ctx.restore();
-            // NOTE: We don't need to release the canvas since it belongs to the
-            // raster now!
-            raster.setCanvas(canvas);
         }
         raster.transform(new Matrix().translate(topLeft.add(size.divide(2)))
                 // Take resolution into account and scale back to original size.
