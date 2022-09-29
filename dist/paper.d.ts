a5df7730db0256b0275c8ef5592f7784cc39a9b1
Jiyon Lee <jiyonlee.d@gmail.com>
2022-09-29 15:41:45 +0900

    Fixed a typo

--- dist/paper.d.ts
+++ dist/paper.d.ts
@@ -5817,7 +5817,7 @@ declare namespace paper {
      * its {@link Path#segments} array.
      * 
      * Each segment consists of an anchor point ({@link Segment#point}) and
-     * optionaly an incoming and an outgoing handle ({@link Segment#handleIn} and
+     * optionally an incoming and an outgoing handle ({@link Segment#handleIn} and
      * {@link Segment#handleOut}), describing the tangents of the two {@link Curve}
      * objects that are connected by this segment.
      */
